2ce4c1a036f210a44fcc06e8bd7910dd462ec246
Jacob Bare <jacob@limit0.io>
2019-09-19 08:11:01 -0500

    Add initial website config, routes, and templates

---
+++ sites/laserfocusworld/config/site.js
@@ -0,0 +1,3 @@
+module.exports = {
+  company: 'Endeavor Business Media, LLC',
+};
---
+++ sites/laserfocusworld/index.js
@@ -0,0 +1,18 @@
+const newrelic = require('newrelic');
+const { startServer } = require('@base-cms/marko-web');
+const { version } = require('./package.json');
+const routes = require('./server/routes');
+const siteConfig = require('./config/site');
+const coreConfig = require('./config/core');
+
+const { log } = console;
+
+module.exports = startServer({
+  rootDir: __dirname,
+  coreConfig,
+  siteConfig,
+  routes,
+  version,
+  onStart: app => app.set('trust proxy', 'loopback, linklocal, uniquelocal'),
+  onAsyncBlockError: e => newrelic.noticeError(e),
+}).then(() => log('Website started!')).catch(e => setImmediate(() => { throw e; }));
