9af6b3b1b0bd17bf1d1f312c1c9e21604add7185
Jacob Bare <jacob@limit0.io>
2019-09-19 10:42:00 -0500

    Add removal todo

--- packages/common/contact-us/index.js
+++ packages/common/contact-us/index.js
@@ -8,6 +8,8 @@ const emailTemplate = require('./email.marko');
 
 const { error } = console;
 
+// @todo This entire service should be deleted once contact us is moved to core.
+
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const exception = (message, code = 400) => {
