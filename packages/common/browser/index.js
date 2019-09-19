2c435a2b41e3ab3b81629fdd931af8db90c4e300
Jacob Bare <jacob@limit0.io>
2019-09-19 10:44:01 -0500

    Create and register contact us form vue component

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,2 +1,6 @@
-export default () => {
+import ContactUsForm from './contact-us-form.vue';
+
+export default (Browser) => {
+  // @todo this should be removed once contact us is moved to core.
+  Browser.registerComponent('CommonContactUsForm', ContactUsForm);
 };
