749bc7018a3db0196b4e9cae28a33c129c772e7a
Brian Miller <bmiller@southcomm.com>
2019-09-24 13:06:54 -0500

    Add an incremented list load more flow
    You can set a query limit and also pass the component and incrementor as to when to split them in to a new list layout.

--- sites/laserfocusworld/server/components.js
+++ sites/laserfocusworld/server/components.js
@@ -1,11 +1,13 @@
 const ContentCardDeckFlow = require('./components/flows/content-card-deck');
 const ContentLoadMoreFlow = require('./components/flows/content-load-more');
+const IncrementedListLoadMoreFlow = require('./components/flows/incremented-list-load-more');
 const ContentListFlow = require('./components/flows/content-list');
 const MagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   ContentCardDeckFlow,
   ContentLoadMoreFlow,
+  IncrementedListLoadMoreFlow,
   ContentListFlow,
   MagazineIssueArchiveFlow,
 };
