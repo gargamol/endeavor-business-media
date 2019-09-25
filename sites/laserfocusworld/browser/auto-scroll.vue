d570ea7ba39422fbdff1a2873db1501ec7be0f92
Brian Miller <bmiller@southcomm.com>
2019-09-25 10:22:49 -0500

    make facet auto scroll more generic

---
+++ sites/laserfocusworld/browser/auto-scroll.vue
@@ -0,0 +1,26 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  props: {
+    containerTarget: {
+      type: String,
+      required: true,
+    },
+    elementTarget: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    const container = document.querySelector(this.containerTarget);
+    const element = document.querySelector(this.elementTarget);
+    if (container && element) {
+      const offset = element.getBoundingClientRect().top - container.getBoundingClientRect().top;
+      container.scrollTop = offset;
+    }
+  },
+};
+</script>
