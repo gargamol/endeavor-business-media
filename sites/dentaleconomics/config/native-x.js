25c2cc639d09590fa654f08b50e6528d4f2c1a1a
Alysha Buselt <>
2019-09-27 11:08:37 -0500

    DE-setup: per BK updated native list to match card

--- sites/dentaleconomics/config/native-x.js
+++ sites/dentaleconomics/config/native-x.js
@@ -5,33 +5,33 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 config
   .setAliasPlacements('default', [
     { name: 'card', id: '5cdb192639937800010cfcca' },
-    { name: 'list2', id: '5cdb192b39937800010cfdf3' },
-    { name: 'list1', id: '5cdb1931f41dfb0001fb3771' },
+    { name: 'list2', id: '5cdb192639937800010cfcca' },
+    { name: 'list1', id: '5cdb192639937800010cfcca' },
   ])
   .setAliasPlacements('practice', [
     { name: 'card', id: '5cdb1959f41dfb0001fb3e06' },
-    { name: 'list2', id: '5cdb1961f41dfb0001fb3f56' },
-    { name: 'list1', id: '5cdb196939937800010d0868' },
+    { name: 'list2', id: '5cdb1959f41dfb0001fb3e06' },
+    { name: 'list1', id: '5cdb1959f41dfb0001fb3e06' },
   ])
   .setAliasPlacements('macro-op-ed', [
     { name: 'card', id: '5cdb193c39937800010d00d9' },
-    { name: 'list2', id: '5cdb1942f41dfb0001fb3a86' },
-    { name: 'list1', id: '5cdb194a39937800010d0323' },
+    { name: 'list2', id: '5cdb193c39937800010d00d9' },
+    { name: 'list1', id: '5cdb193c39937800010d00d9' },
   ])
   .setAliasPlacements('science-tech', [
     { name: 'card', id: '5cdb1974f41dfb0001fb42de' },
-    { name: 'list2', id: '5cdb197bf41dfb0001fb4472' },
-    { name: 'list1', id: '5cdb1982f41dfb0001fb4603' },
+    { name: 'list2', id: '5cdb1974f41dfb0001fb42de' },
+    { name: 'list1', id: '5cdb1974f41dfb0001fb42de' },
   ])
   .setAliasPlacements('money', [
     { name: 'card', id: '5cdb198c39937800010d0f7b' },
-    { name: 'list2', id: '5cdb199339937800010d10c5' },
-    { name: 'list1', id: '5cdb199bf41dfb0001fb4b06' },
+    { name: 'list2', id: '5cdb198c39937800010d0f7b' },
+    { name: 'list1', id: '5cdb198c39937800010d0f7b' },
   ])
   .setAliasPlacements('dental-office-design', [
     { name: 'card', id: '5cdb19aaf41dfb0001fb4d52' },
-    { name: 'list2', id: '5cdb19b339937800010d15e5' },
-    { name: 'list1', id: '5cdb19bcf41dfb0001fb506d' },
+    { name: 'list2', id: '5cdb19aaf41dfb0001fb4d52' },
+    { name: 'list1', id: '5cdb19aaf41dfb0001fb4d52' },
   ]);
 
 module.exports = config;
