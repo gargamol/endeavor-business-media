bb99cf6f20863f31def010035a7f35c462a8f855
Brandon Krigbaum <brandonbk@gmail.com>
2019-09-27 10:50:10 -0500

    Updated site and navigation configs

--- sites/lightwaveonline/config/site.js
+++ sites/lightwaveonline/config/site.js
@@ -5,25 +5,25 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=45',
+      src: 'https://base.imgix.net/files/base/pennwell/lw/logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=90 2x',
+        'https://base.imgix.net/files/base/pennwell/lw/logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=60',
+      src: 'https://base.imgix.net/files/base/pennwell/lw/logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=120 2x',
+        'https://base.imgix.net/files/base/pennwell/lw/logo.png?h=120 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'linkedin', href: 'http://www.linkedin.com/groups/Laser-Focus-World-2896249' },
-    { provider: 'twitter', href: 'https://twitter.com/LaserFocusWorld' },
-    { provider: 'facebook', href: 'https://www.facebook.com/pages/Laser-Focus-World/126899915297' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/4000932', target: '_blank' },
+    { provider: 'twitter', href: 'https://twitter.com/lightwaveonline', target: '_blank' },
+    { provider: 'facebook', href: 'https://www.facebook.com/LightwaveOnline', target: '_blank' },
   ],
   gtm: {
-    containerId: 'GTM-M7H8VJG',
+    containerId: 'GTM-M3B7WXJ',
   },
   wufoo: {
     userName: 'cygnuscorporate',
