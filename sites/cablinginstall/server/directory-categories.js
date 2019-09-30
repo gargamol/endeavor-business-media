db61b0a5daa6299ba1759a2158b3522ae22d831d
Brian Miller <bmiller@southcomm.com>
2019-09-30 14:08:02 -0500

    add directory page

---
+++ sites/cablinginstall/server/directory-categories.js
@@ -0,0 +1,439 @@
+module.exports = [
+  {
+    id: 34301,
+    name: 'Manufacturer Products',
+    href: '/directory/manufacturer-products',
+    values: [
+      {
+        id: 34300,
+        name: 'Active Infrastructure Devices',
+        href: '/directory/manufacturer-products/active-infrastructure-devices',
+        values: [
+          {
+            id: 34299,
+            name: 'Media Converters',
+            href: '/directory/manufacturer-products/active-infrastructure-devices/media-converters',
+          },
+          {
+            id: 34303,
+            name: 'Power-over-Ethernet Powering Devices',
+            href: '/directory/manufacturer-products/active-infrastructure-devices/power-over-ethernet-powering-devices',
+          },
+          {
+            id: 34304,
+            name: 'Transceivers',
+            href: '/directory/manufacturer-products/active-infrastructure-devices/transceivers',
+          },
+        ],
+      },
+      {
+        id: 34306,
+        name: 'Cable Protection and Management',
+        href: '/directory/manufacturer-products/cable-protection-and-management',
+        values: [
+          {
+            id: 34305,
+            name: 'Cable Marking/Labeling Equipment',
+            href: '/directory/manufacturer-products/cable-protection-and-management/cable-marking-labeling-equipment',
+          },
+          {
+            id: 34307,
+            name: 'Cable-management Software',
+            href: '/directory/manufacturer-products/cable-protection-and-management/cable-management-software',
+          },
+          {
+            id: 34308,
+            name: 'Firestopping Devices and Materials',
+            href: '/directory/manufacturer-products/cable-protection-and-management/firestopping-devices-and-materials',
+          },
+          {
+            id: 34309,
+            name: 'Grounding/Bonding Equipment',
+            href: '/directory/manufacturer-products/cable-protection-and-management/grounding-bonding-equipment',
+          },
+          {
+            id: 34310,
+            name: 'Surge-protection Devices',
+            href: '/directory/manufacturer-products/cable-protection-and-management/surge-protection-devices',
+          },
+          {
+            id: 34311,
+            name: 'Uninterruptible Power Supplies',
+            href: '/directory/manufacturer-products/cable-protection-and-management/uninterruptible-power-supplies',
+          },
+        ],
+      },
+      {
+        id: 34313,
+        name: 'Cabling Media',
+        href: '/directory/manufacturer-products/cabling-media',
+        values: [
+          {
+            id: 34312,
+            name: 'Blown Fiber Systems',
+            href: '/directory/manufacturer-products/cabling-media/blown-fiber-systems',
+          },
+          {
+            id: 34314,
+            name: 'Coaxial Cable',
+            href: '/directory/manufacturer-products/cabling-media/coaxial-cable',
+          },
+          {
+            id: 34315,
+            name: 'Composite Copper/Fiber Cable',
+            href: '/directory/manufacturer-products/cabling-media/composite-copper-fiber-cable',
+          },
+          {
+            id: 34316,
+            name: 'End-to-end Cabling Systems',
+            href: '/directory/manufacturer-products/cabling-media/end-to-end-cabling-systems',
+          },
+          {
+            id: 34317,
+            name: 'Hybrid Fiber-optic Cable',
+            href: '/directory/manufacturer-products/cabling-media/hybrid-fiber-optic-cable',
+          },
+          {
+            id: 34318,
+            name: 'Indoor/Outdoor Fiber-optic Cable',
+            href: '/directory/manufacturer-products/cabling-media/indoor-outdoor-fiber-optic-cable',
+          },
+          {
+            id: 34319,
+            name: 'Multimode Fiber-optic Cable',
+            href: '/directory/manufacturer-products/cabling-media/multimode-fiber-optic-cable',
+          },
+          {
+            id: 34320,
+            name: 'Patch Cords/Jumpers/Assemblies',
+            href: '/directory/manufacturer-products/cabling-media/patch-cords-jumpers-assemblies',
+          },
+          {
+            id: 34321,
+            name: 'Shielded Twisted-pair Cable',
+            href: '/directory/manufacturer-products/cabling-media/shielded-twisted-pair-cable',
+          },
+          {
+            id: 34322,
+            name: 'Singlemode Fiber-optic Cable',
+            href: '/directory/manufacturer-products/cabling-media/singlemode-fiber-optic-cable',
+          },
+          {
+            id: 34323,
+            name: 'Unshielded Twisted-pair Cable',
+            href: '/directory/manufacturer-products/cabling-media/unshielded-twisted-pair-cable',
+          },
+          {
+            id: 34324,
+            name: 'Wireless LAN Access Points',
+            href: '/directory/manufacturer-products/cabling-media/wireless-lan-access-points',
+          },
+        ],
+      },
+      {
+        id: 34326,
+        name: 'Connection & Cross Connection Devices',
+        href: '/directory/manufacturer-products/connection-cross-connection-devices',
+        values: [
+          {
+            id: 34325,
+            name: 'Adapters',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/adapters',
+          },
+          {
+            id: 34327,
+            name: 'Baluns',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/baluns',
+          },
+          {
+            id: 34328,
+            name: 'Coaxial Connectors',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/coaxial-connectors',
+          },
+          {
+            id: 34329,
+            name: 'Fiber-optic Connectors',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/fiber-optic-connectors',
+          },
+          {
+            id: 34330,
+            name: 'IDC  Blocks',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/idc-blocks',
+          },
+          {
+            id: 34331,
+            name: 'Passive Fiber-optic Components',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/passive-fiber-optic-components',
+          },
+          {
+            id: 34332,
+            name: 'Patch Panels',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/patch-panels',
+          },
+          {
+            id: 34333,
+            name: 'Shielded Twisted-pair Connectors',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/shielded-twisted-pair-connectors',
+          },
+          {
+            id: 34334,
+            name: 'Unshielded Twisted-pair Connectors',
+            href: '/directory/manufacturer-products/connection-cross-connection-devices/unshielded-twisted-pair-connectors',
+          },
+        ],
+      },
+      {
+        id: 34336,
+        name: 'Distribution and Routing Equipment',
+        href: '/directory/manufacturer-products/distribution-and-routing-equipment',
+        values: [
+          {
+            id: 34335,
+            name: 'Cable Support Devices',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/cable-support-devices',
+          },
+          {
+            id: 34337,
+            name: 'Cable Ties',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/cable-ties',
+          },
+          {
+            id: 34338,
+            name: 'Cable Tray',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/cable-tray',
+          },
+          {
+            id: 34339,
+            name: 'Cable-pulling Lubricants',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/cable-pulling-lubricants',
+          },
+          {
+            id: 34340,
+            name: 'Conduit',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/conduit',
+          },
+          {
+            id: 34341,
+            name: 'Ducts and Raceways',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/ducts-and-raceways',
+          },
+          {
+            id: 34342,
+            name: 'Enclosures',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/enclosures',
+          },
+          {
+            id: 34343,
+            name: 'Innerduct',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/innerduct',
+          },
+          {
+            id: 34344,
+            name: 'Racks',
+            href: '/directory/manufacturer-products/distribution-and-routing-equipment/racks',
+          },
+        ],
+      },
+      {
+        id: 34356,
+        name: 'Specialized Equipment & Materials',
+        href: '/directory/manufacturer-products/specialized-equipment-materials',
+        values: [
+          {
+            id: 34355,
+            name: 'Cable-pulling Equipment',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/cable-pulling-equipment',
+          },
+          {
+            id: 34357,
+            name: 'Chemicals and Materials',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/chemicals-and-materials',
+          },
+          {
+            id: 34358,
+            name: 'Estimating Software',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/estimating-software',
+          },
+          {
+            id: 34359,
+            name: 'Safety Equipment',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/safety-equipment',
+          },
+          {
+            id: 34360,
+            name: 'Security Products',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/security-products',
+          },
+          {
+            id: 34361,
+            name: 'Specialized Tools',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/specialized-tools',
+          },
+          {
+            id: 34362,
+            name: 'Wireless LAN Site Survey/Coverage Software',
+            href: '/directory/manufacturer-products/specialized-equipment-materials/wireless-lan-site-survey-coverage-software',
+          },
+        ],
+      },
+      {
+        id: 34364,
+        name: 'Splicing And Termination Equipment',
+        href: '/directory/manufacturer-products/splicing-and-termination-equipment',
+        values: [
+          {
+            id: 34363,
+            name: 'Boxes',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/boxes',
+          },
+          {
+            id: 34365,
+            name: 'Fusion Splicers',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/fusion-splicers',
+          },
+          {
+            id: 34366,
+            name: 'Mechanical Splices',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/mechanical-splices',
+          },
+          {
+            id: 34367,
+            name: 'Multi-user Telecommunications Outlet Assemblies (MUTOAs)',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/multi-user-telecommunications-outlet-assemblies-mutoas',
+          },
+          {
+            id: 34368,
+            name: 'Outlets',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/outlets',
+          },
+          {
+            id: 34369,
+            name: 'Splice Closures/Trays',
+            href: '/directory/manufacturer-products/splicing-and-termination-equipment/splice-closures-trays',
+          },
+        ],
+      },
+      {
+        id: 34371,
+        name: 'Test Equipment',
+        href: '/directory/manufacturer-products/test-equipment',
+        values: [
+          {
+            id: 34370,
+            name: 'Butt Test Sets',
+            href: '/directory/manufacturer-products/test-equipment/butt-test-sets',
+          },
+          {
+            id: 34372,
+            name: 'LAN Cable Testers',
+            href: '/directory/manufacturer-products/test-equipment/lan-cable-testers',
+          },
+          {
+            id: 34373,
+            name: 'Multimeters',
+            href: '/directory/manufacturer-products/test-equipment/multimeters',
+          },
+          {
+            id: 34374,
+            name: 'Optical Faultfinders/Visual Fault Locators',
+            href: '/directory/manufacturer-products/test-equipment/optical-faultfinders-visual-fault-locators',
+          },
+          {
+            id: 34375,
+            name: 'Optical Talk Sets',
+            href: '/directory/manufacturer-products/test-equipment/optical-talk-sets',
+          },
+          {
+            id: 34376,
+            name: 'Optical Time-domain Reflectometers',
+            href: '/directory/manufacturer-products/test-equipment/optical-time-domain-reflectometers',
+          },
+          {
+            id: 34377,
+            name: 'Optical-loss Test Sets',
+            href: '/directory/manufacturer-products/test-equipment/optical-loss-test-sets',
+          },
+          {
+            id: 34378,
+            name: 'Protocol-specific Testers',
+            href: '/directory/manufacturer-products/test-equipment/protocol-specific-testers',
+          },
+          {
+            id: 34379,
+            name: 'Tone Generators/Probes',
+            href: '/directory/manufacturer-products/test-equipment/tone-generators-probes',
+          },
+          {
+            id: 34380,
+            name: 'Wireless Test Equipment',
+            href: '/directory/manufacturer-products/test-equipment/wireless-test-equipment',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 34346,
+    name: 'Distributor Products',
+    href: '/directory/distributor-products',
+    values: [
+      {
+        id: 34345,
+        name: 'Active Infrastructure Devices Distributors',
+        href: '/directory/distributor-products/active-infrastructure-devices-distributors',
+      },
+      {
+        id: 34347,
+        name: 'Cable Protection and Management Distributors',
+        href: '/directory/distributor-products/cable-protection-and-management-distributors',
+      },
+      {
+        id: 34348,
+        name: 'Cabling Media Distributors',
+        href: '/directory/distributor-products/cabling-media-distributors',
+      },
+      {
+        id: 34349,
+        name: 'Connection and Crossconnection Devices Distributors',
+        href: '/directory/distributor-products/connection-and-crossconnection-devices-distributors',
+      },
+      {
+        id: 34350,
+        name: 'Distribution and Routing Equipment Distributors',
+        href: '/directory/distributor-products/distribution-and-routing-equipment-distributors',
+      },
+      {
+        id: 34351,
+        name: 'Distributor Customer Services',
+        href: '/directory/distributor-products/distributor-customer-services',
+      },
+      {
+        id: 34352,
+        name: 'Specialized Equipment & Materials Distributors',
+        href: '/directory/distributor-products/specialized-equipment-materials-distributors',
+      },
+      {
+        id: 34353,
+        name: 'Splicing and Termination Equipment Distributors',
+        href: '/directory/distributor-products/splicing-and-termination-equipment-distributors',
+      },
+      {
+        id: 34354,
+        name: 'Test Equipment Distributors',
+        href: '/directory/distributor-products/test-equipment-distributors',
+      },
+    ],
+  },
+  {
+    id: 34382,
+    name: 'Services',
+    href: '/directory/services',
+    values: [
+      {
+        id: 34381,
+        name: 'Training and Education',
+        href: '/directory/services/training-and-education',
+      },
+    ],
+  },
+];
