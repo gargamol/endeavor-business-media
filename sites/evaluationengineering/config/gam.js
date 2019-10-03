f531fc2ab2679bf31f7043571f34a7c208061d8a
Alysha Buselt <>
2019-10-03 15:31:08 -0500

    EE-transfer: config and public files
    Note — EE uses bing search, which is not yet supported in new framework
    
    Removed GCSE and NativeX as they are not applicable currently.

--- sites/evaluationengineering/config/gam.js
+++ sites/evaluationengineering/config/gam.js
@@ -1,6 +1,6 @@
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'LFW' });
+const config = new GAMConfiguration('21687441225', { basePath: 'EE' });
 
 config
   .setTemplate('LB', {
@@ -23,53 +23,45 @@ config
     { name: 'reskin', path: 'default/reskin' },
     { name: 'wa', path: 'default/wa' },
   ])
-  .setAliasAdUnits('detectors-imaging', [
-    { name: 'lb1', templateName: 'LB', path: 'detectors-imaging/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'detectors-imaging/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'detectors-imaging/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'detectors-imaging/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'detectors-imaging/load-more' },
-    { name: 'reskin', path: 'detectors-imaging/reskin' },
+  .setAliasAdUnits('data-center', [
+    { name: 'lb1', templateName: 'LB', path: 'data-center/lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'data-center/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'data-center/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'data-center/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'data-center/load-more' },
+    { name: 'reskin', path: 'data-center/reskin' },
   ])
-  .setAliasAdUnits('lasers-sources', [
-    { name: 'lb1', templateName: 'LB', path: 'lasers-sources/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'lasers-sources/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'lasers-sources/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'lasers-sources/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'lasers-sources/load-more' },
-    { name: 'reskin', path: 'lasers-sources/reskin' },
+  .setAliasAdUnits('applications', [
+    { name: 'lb1', templateName: 'LB', path: 'applications/lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'applications/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'applications/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'applications/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'applications/load-more' },
+    { name: 'reskin', path: 'applications/reskin' },
   ])
-  .setAliasAdUnits('optics', [
-    { name: 'lb1', templateName: 'LB', path: 'optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'optics/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'optics/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'optics/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'optics/load-more' },
-    { name: 'reskin', path: 'optics/reskin' },
+  .setAliasAdUnits('industries', [
+    { name: 'lb1', templateName: 'LB', path: 'industries/lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'industries/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'industries/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'industries/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'industries/load-more' },
+    { name: 'reskin', path: 'industries/reskin' },
   ])
-  .setAliasAdUnits('fiber-optics', [
-    { name: 'lb1', templateName: 'LB', path: 'fiber-optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'fiber-optics/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'fiber-optics/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'fiber-optics/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'fiber-optics/load-more' },
-    { name: 'reskin', path: 'fiber-optics/reskin' },
+  .setAliasAdUnits('instrumentation', [
+    { name: 'lb1', templateName: 'LB', path: 'instrumentation/lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'instrumentation/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'instrumentation/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'instrumentation/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'instrumentation/load-more' },
+    { name: 'reskin', path: 'instrumentation/reskin' },
   ])
-  .setAliasAdUnits('software-accessories', [
-    { name: 'lb1', templateName: 'LB', path: 'software-accessories/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'software-accessories/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'software-accessories/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'software-accessories/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'software-accessories/load-more' },
-    { name: 'reskin', path: 'software-accessories/reskin' },
-  ])
-  .setAliasAdUnits('test-measurement', [
-    { name: 'lb1', templateName: 'LB', path: 'test-measurement/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'test-measurement/lb2' },
-    { name: 'rail1', templateName: 'CONTENT', path: 'test-measurement/rail1' },
-    { name: 'rail2', templateName: 'CONTENT', path: 'test-measurement/rail2' },
-    { name: 'load-more', templateName: 'CONTENT', path: 'test-measurement/load-more' },
-    { name: 'reskin', path: 'test-measurement/reskin' },
+  .setAliasAdUnits('test-issues-techniques', [
+    { name: 'lb1', templateName: 'LB', path: 'test-issues-techniques/lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'test-issues-techniques/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'test-issues-techniques/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'test-issues-techniques/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'test-issues-techniques/load-more' },
+    { name: 'reskin', path: 'test-issues-techniques/reskin' },
   ]);
 
 module.exports = config;
--- sites/evaluationengineering/config/site.js
+++ sites/evaluationengineering/config/site.js
@@ -5,25 +5,25 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=45',
+      src: 'https://base.imgix.net/files/base/pennwell/ee/logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=90 2x',
+        'https://base.imgix.net/files/base/pennwell/ee/logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=60',
+      src: 'https://base.imgix.net/files/base/pennwell/ee/logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=120 2x',
+        'https://base.imgix.net/files/base/pennwell/ee/logo.png?h=120 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/2896249/', target: '_blank' },
-    { provider: 'twitter', href: 'https://twitter.com/LaserFocusWorld', target: '_blank' },
-    { provider: 'facebook', href: 'https://www.facebook.com/pages/Laser-Focus-World/126899915297', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/2192877/profile' },
+    { provider: 'twitter', href: 'https://twitter.com/EE_Engineers' },
+    { provider: 'facebook', href: 'https://www.facebook.com/EvaluationEngineering' },
   ],
   gtm: {
-    containerId: 'GTM-MFCT2LV',
+    containerId: 'GTM-NR2DFJQ',
   },
   wufoo: {
     userName: 'cygnuscorporate',
@@ -33,9 +33,9 @@ module.exports = {
   },
   contactUs: {
     branding: {
-      bgColor: '#164f77',
-      logo: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=60',
+      bgColor: '#d71920',
+      logo: 'https://base.imgix.net/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
     },
-    to: 'juliac@pennwell.com',
+    to: 'kcormier@evaluationengineering.com',
   },
 };
