c3d77b0d25fba47d1ccecc6f2bcb2394c6b8d628
Brian Miller <bmiller@southcomm.com>
2019-10-08 09:57:17 -0500

    all img scr now reference img.cablinginstall.com

--- sites/cablinginstall/config/site.js
+++ sites/cablinginstall/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/pennwell/cim/cim-header-logo-2.png?h=45',
+      src: 'https://img.cablinginstall.com/files/base/pennwell/cim/cim-header-logo-2.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/cim/cim-header-logo-2.png?h=90 2x',
+        'https://img.cablinginstall.com/files/base/pennwell/cim/cim-header-logo-2.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/pennwell/cim/logo.png?h=60',
+      src: 'https://img.cablinginstall.com/files/base/pennwell/cim/logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/pennwell/cim/logo.png?h=120 2x',
+        'https://img.cablinginstall.com/files/base/pennwell/cim/logo.png?h=120 2x',
       ],
     },
   },
@@ -31,7 +31,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#ee2e30',
-      logo: 'https://base.imgix.net/files/base/pennwell/cim/logo.png?h=60',
+      logo: 'https://img.cablinginstall.com/files/base/pennwell/cim/logo.png?h=60',
     },
     to: 'juliac@pennwell.com',
   },
@@ -41,7 +41,7 @@ module.exports = {
     sendTo: 'juliac@pennwell.com',
     sendFrom: 'CablingInstall.com <noreply@baseplatform.io>',
     sendBcc: 'emailactivity@cygnus.com',
-    logo: 'https://base.imgix.net/files/base/pennwell/cim/logo.png?h=60',
+    logo: 'https://img.cablinginstall.com/files/base/pennwell/cim/logo.png?h=60',
     bgColor: '#ee2e30',
   },
   magazines: {
