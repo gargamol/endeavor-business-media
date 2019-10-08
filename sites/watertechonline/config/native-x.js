01c3a3df7be7486eaa19c5d20c8ffa09c45bc3b7
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-08 08:18:16 -0500

    Updated native-x config and associated templates

--- sites/watertechonline/config/native-x.js
+++ sites/watertechonline/config/native-x.js
@@ -4,34 +4,22 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'card', id: '5d96036066f22a000172bd75' },
-    { name: 'list2', id: '5d96036066f22a000172bd75' },
-    { name: 'list1', id: '5d96036066f22a000172bd75' },
+    { name: 'default', id: '5d96036066f22a000172bd75' },
   ])
   .setAliasPlacements('process-water', [
-    { name: 'card', id: '5d960375da2aa1000117e542' },
-    { name: 'list2', id: '5d960375da2aa1000117e542' },
-    { name: 'list1', id: '5d960375da2aa1000117e542' },
+    { name: 'default', id: '5d960375da2aa1000117e542' },
   ])
   .setAliasPlacements('wastewater', [
-    { name: 'card', id: '5d9608c866f22a0001753b1d' },
-    { name: 'list2', id: '5d9608c866f22a0001753b1d' },
-    { name: 'list1', id: '5d9608c866f22a0001753b1d' },
+    { name: 'default', id: '5d9608c866f22a0001753b1d' },
   ])
   .setAliasPlacements('water-reuse', [
-    { name: 'card', id: '5d9608d4da2aa100011a5f26' },
-    { name: 'list2', id: '5d9608d4da2aa100011a5f26' },
-    { name: 'list1', id: '5d9608d4da2aa100011a5f26' },
+    { name: 'default', id: '5d9608d4da2aa100011a5f26' },
   ])
   .setAliasPlacements('produced-water', [
-    { name: 'card', id: '5d9608e0da2aa100011a6543' },
-    { name: 'list2', id: '5d9608e0da2aa100011a6543' },
-    { name: 'list1', id: '5d9608e0da2aa100011a6543' },
+    { name: 'default', id: '5d9608e0da2aa100011a6543' },
   ])
   .setAliasPlacements('industry', [
-    { name: 'card', id: '5d9608ef66f22a0001754d41' },
-    { name: 'list2', id: '5d9608ef66f22a0001754d41' },
-    { name: 'list1', id: '5d9608ef66f22a0001754d41' },
+    { name: 'default', id: '5d9608ef66f22a0001754d41' },
   ]);
 
 module.exports = config;
