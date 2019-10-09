10dcb88ab3e614074e6142dce9b6b0bdae926468
Brian Miller <bmiller@southcomm.com>
2019-10-09 13:41:08 -0500

    fix bg color

--- sites/lightwaveonline/config/site.js
+++ sites/lightwaveonline/config/site.js
@@ -45,6 +45,6 @@ module.exports = {
     sendFrom: 'LaserFocusWorld.com <noreply@baseplatform.io>',
     sendBcc: 'emailactivity@cygnus.com',
     logo: 'https://img.lightwaveonline.com/files/base/pennwell/lw/logo.png?h=60',
-    bgColor: '#106d7d',
+    bgColor: '#fff',
   },
 };
