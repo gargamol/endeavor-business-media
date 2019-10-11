38c0576d12a09af8c3fe60c52c006eff0491d399
Alysha Buselt <>
2019-10-11 13:37:56 -0500

    CS3177-VSD: restore content whitepaper settings

--- sites/vision-systems/server/routes/content.js
+++ sites/vision-systems/server/routes/content.js
@@ -2,10 +2,15 @@ const { withContent } = require('@base-cms/marko-web/middleware');
 const company = require('../templates/content/company');
 const content = require('../templates/content');
 const queryFragment = require('../graphql/fragments/content-page');
+const whitepaper = require('../templates/content/whitepaper');
 
 module.exports = (app) => {
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
+    queryFragment,
+  }));
+  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
+    template: whitepaper,
     queryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
