8de5d57afc89db0f793bfae392008af606030484
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-13 12:41:55 -0500

    Updated native-x config

--- sites/bioopticsworld/config/native-x.js
+++ sites/bioopticsworld/config/native-x.js
@@ -4,34 +4,22 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'card', id: '5cdb1e92f41dfb0001fc3418' },
-    { name: 'list2', id: '5cdb1e92f41dfb0001fc3418' },
-    { name: 'list1', id: '5cdb1e92f41dfb0001fc3418' },
+    { name: 'default', id: '5cdb1e92f41dfb0001fc3418' },
   ])
   .setAliasPlacements('biophotonics-tools', [
-    { name: 'card', id: '5cdb1f3939937800010e1083' },
-    { name: 'list2', id: '5cdb1f3939937800010e1083' },
-    { name: 'list1', id: '5cdb1f3939937800010e1083' },
+    { name: 'default', id: '5cdb1f3939937800010e1083' },
   ])
   .setAliasPlacements('biomedicine', [
-    { name: 'card', id: '5cdb1ed7f41dfb0001fc3ea7' },
-    { name: 'list2', id: '5cdb1ed7f41dfb0001fc3ea7' },
-    { name: 'list1', id: '5cdb1ed7f41dfb0001fc3ea7' },
+    { name: 'default', id: '5cdb1ed7f41dfb0001fc3ea7' },
   ])
   .setAliasPlacements('bioimaging', [
-    { name: 'card', id: '5cdb1ef3f41dfb0001fc42db' },
-    { name: 'list2', id: '5cdb1ef3f41dfb0001fc42db' },
-    { name: 'list1', id: '5cdb1ef3f41dfb0001fc42db' },
+    { name: 'default', id: '5cdb1ef3f41dfb0001fc42db' },
   ])
   .setAliasPlacements('bioscience', [
-    { name: 'card', id: '5cdb1ebdf41dfb0001fc3abb' },
-    { name: 'list2', id: '5cdb1ebdf41dfb0001fc3abb' },
-    { name: 'list1', id: '5cdb1ebdf41dfb0001fc3abb' },
+    { name: 'default', id: '5cdb1ebdf41dfb0001fc3abb' },
   ])
   .setAliasPlacements('biophotonics-techniques', [
-    { name: 'card', id: '5cdb1f0cf41dfb0001fc4710' },
-    { name: 'list2', id: '5cdb1f0cf41dfb0001fc4710' },
-    { name: 'list1', id: '5cdb1f0cf41dfb0001fc4710' },
+    { name: 'default', id: '5cdb1f0cf41dfb0001fc4710' },
   ]);
 
 module.exports = config;
