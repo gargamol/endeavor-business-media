a978e695b8695ee292ac56f091785ed38f719425
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-14 07:59:17 -0500

    Updated native-x config

--- sites/strategies-u/config/native-x.js
+++ sites/strategies-u/config/native-x.js
@@ -4,29 +4,19 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'card', id: '5cdc6f7bf41dfb00012645e4' },
-    { name: 'list2', id: '5cdc6f7bf41dfb00012645e4' },
-    { name: 'list1', id: '5cdc6f7bf41dfb00012645e4' },
+    { name: 'default', id: '5cdc6f7bf41dfb00012645e4' },
   ])
   .setAliasPlacements('reports', [
-    { name: 'card', id: '5cdc6f91399378000137df3c' },
-    { name: 'list2', id: '5cdc6f91399378000137df3c' },
-    { name: 'list1', id: '5cdc6f91399378000137df3c' },
+    { name: 'default', id: '5cdc6f91399378000137df3c' },
   ])
   .setAliasPlacements('analysts', [
-    { name: 'card', id: '5cdc6fb6399378000137e860' },
-    { name: 'list2', id: '5cdc6fb6399378000137e860' },
-    { name: 'list1', id: '5cdc6fb6399378000137e860' },
+    { name: 'default', id: '5cdc6fb6399378000137e860' },
   ])
   .setAliasPlacements('blogs', [
-    { name: 'card', id: '5cdc6fbff41dfb000126561d' },
-    { name: 'list2', id: '5cdc6fbff41dfb000126561d' },
-    { name: 'list1', id: '5cdc6fbff41dfb000126561d' },
+    { name: 'default', id: '5cdc6fbff41dfb000126561d' },
   ])
   .setAliasPlacements('press-releases', [
-    { name: 'card', id: '5cdc6fd6399378000137f17d' },
-    { name: 'list2', id: '5cdc6fd6399378000137f17d' },
-    { name: 'list1', id: '5cdc6fd6399378000137f17d' },
+    { name: 'default', id: '5cdc6fd6399378000137f17d' },
   ]);
 
 module.exports = config;
