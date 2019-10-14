539d812fcd2f7c912c926ba70baa1b182d96090e
Alysha Buselt <>
2019-10-14 13:26:30 -0500

    CS3232-RDH-RSS
    https://southcomm.atlassian.net/browse/CS-3232
    
    - Added RSS feed to right rail, per BK’s approval.
    - Added sponsor image below RSS feed
    - Updated styling of RSS header to match other headers

--- sites/rdhmag/server/routes/website-section.js
+++ sites/rdhmag/server/routes/website-section.js
@@ -1,6 +1,7 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
+const jobPostings = require('../templates/website-section/job-postings');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
@@ -8,7 +9,10 @@ module.exports = (app) => {
     template: contactUs,
     queryFragment,
   }));
-
+  app.get('/:alias(job-postings)', withWebsiteSection({
+    template: jobPostings,
+    queryFragment,
+  }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
