8014d196374fe766269bde16e3dd8cd28e0d8654
Brian Miller <bmiller@southcomm.com>
2019-10-22 10:57:07 -0500

    cloned FCN to FRN

---
+++ sites/foresternetwork/config/navigation.js
@@ -0,0 +1,70 @@
+module.exports = {
+  primary: {
+    items: [
+      { href: '/instrumentation', label: 'Instrumentation & Measurement' },
+      { href: '/valves-actuators', label: 'Valves & Actuators' },
+      { href: '/bearings-seals', label: 'Bearings & Seals' },
+      { href: '/piping-tubing-hosing', label: 'Piping, Tubing & Hosing' },
+      { href: '/process-control-automation', label: 'Process Control' },
+      { href: '/pumps-motors-drives', label: 'Pumps, Motors & Drives' },
+    ],
+  },
+  secondary: {
+    items: [
+      { href: '/subscribe', label: 'Subscribe' },
+      { href: '/magazine', label: 'Magazine' },
+      { href: '/webinars', label: 'Webinars' },
+      { href: '/events', label: 'Events' },
+      { href: '/news-reports', label: 'News & Reports' },
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/page/about-us', label: 'About Us' },
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: 'https://www.processflownetwork.com', label: 'Advertise', target: '_blank' },
+      { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+      { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+        { href: '/instrumentation', label: 'Instrumentation & Measurement' },
+        { href: '/valves-actuators', label: 'Valves & Actuators' },
+        { href: '/bearings-seals', label: 'Bearings & Seals' },
+        { href: '/piping-tubing-hosing', label: 'Piping, Tubing & Hosing' },
+        { href: '/process-control-automation', label: 'Process Control & Automation' },
+        { href: '/pumps-motors-drives', label: 'Pumps, Motors & Drives' },
+        { href: '/maintenance-safety', label: 'Maintenance & Safety' },
+        { href: '/industry-applications', label: 'Industry Applications' },
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/webinars', label: 'Webinars' },
+        { href: '/events', label: 'Events' },
+        { href: '/news-reports', label: 'News & Reports' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: 'https://www.processflownetwork.com', label: 'Advertise', target: '_blank' },
+        { href: '/page/about-us', label: 'About Us' },
+        { href: '/contact-us', label: 'Contact Us' },
+        { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+        { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
+      ],
+    },
+  ],
+};
