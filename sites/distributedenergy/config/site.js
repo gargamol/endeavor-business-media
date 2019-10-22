e1bfe0e5251e5a7eed931f1ef8d8f8b40baca12b
Brian Miller <bmiller@southcomm.com>
2019-10-22 18:52:45 -0500

    clone site to dte

---
+++ sites/distributedenergy/config/site.js
@@ -0,0 +1,41 @@
+const navigation = require('./navigation');
+
+module.exports = {
+  navigation,
+  company: 'Endeavor Business Media, LLC',
+  logos: {
+    navbar: {
+      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=45',
+      srcset: [
+        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=90 2x',
+      ],
+    },
+    footer: {
+      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      srcset: [
+        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=120 2x',
+      ],
+    },
+  },
+  socialMediaLinks: [
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/forester_media_inc-/' },
+    { provider: 'twitter', href: 'https://twitter.com/foresternetwork' },
+    { provider: 'facebook', href: 'https://www.facebook.com/foresternetwork' },
+  ],
+  gtm: {
+    containerId: 'GTM-TNFLPD9',
+  },
+  wufoo: {
+    userName: 'cygnuscorporate',
+  },
+  magazines: {
+    description: '',
+  },
+  contactUs: {
+    branding: {
+      bgColor: '#164f77',
+      logo: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+    },
+    to: 'adsales@endeavorb2b.com',
+  },
+};
