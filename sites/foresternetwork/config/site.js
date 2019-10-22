21b41b8edf8aeaafc3cb6248e85b704959757ef2
Brian Miller <bmiller@southcomm.com>
2019-10-22 12:55:42 -0500

    update site configs

--- sites/foresternetwork/config/site.js
+++ sites/foresternetwork/config/site.js
@@ -5,25 +5,25 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/ebm/fcn/image/static/fcn_logo_og.png?h=45',
+      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/fcn/image/static/fcn_logo_og.png?h=90 2x',
+        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/ebm/fcn/image/static/fcn_logo_og.png?h=60',
+      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/fcn/image/static/fcn_logo_og.png?h=120 2x',
+        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/4068011', target: '_blank' },
-    { provider: 'twitter', href: 'https://twitter.com/flowcontrol_mag', target: '_blank' },
-    { provider: 'facebook', href: 'https://www.facebook.com/FlowControlMagazine', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/forester_media_inc-/' },
+    { provider: 'twitter', href: 'https://twitter.com/foresternetwork' },
+    { provider: 'facebook', href: 'https://www.facebook.com/foresternetwork' },
   ],
   gtm: {
-    containerId: 'GTM-MNTKWH8',
+    gtmContainer: 'GTM-TNFLPD9',
   },
   wufoo: {
     userName: 'cygnuscorporate',
@@ -34,7 +34,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://base.imgix.net/files/base/ebm/fcn/image/static/fcn_logo_og.png?h=60',
+      logo: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
     },
     // to: '',
   },
