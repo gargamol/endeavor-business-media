ec3c653cd419fa7c4f19c3a3aa6efac73cef6c3e
Brian Miller <bmiller@southcomm.com>
2019-10-23 08:43:50 -0500

    udpate gam ad units

--- sites/mswmanagement/config/gam.js
+++ sites/mswmanagement/config/gam.js
@@ -30,6 +30,51 @@ config
     { name: 'load-more', templateName: 'CONTENT', path: 'default/load-more' },
     { name: 'reskin', path: 'default/reskin' },
     { name: 'wa', path: 'default/wa' },
+  ])
+  .setAliasAdUnits('collection', [
+    { name: 'lb1', templateName: 'LB', path: 'collection/lb1' },
+    { name: 'lb2', templateName: 'LB2', path: 'collection/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'collection/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'collection/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'collection/load-more' },
+    { name: 'reskin', path: 'collection/reskin' },
+    { name: 'wa', path: 'collection/wa' },
+  ])
+  .setAliasAdUnits('landfills', [
+    { name: 'lb1', templateName: 'LB', path: 'landfills/lb1' },
+    { name: 'lb2', templateName: 'LB2', path: 'landfills/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'landfills/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'landfills/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'landfills/load-more' },
+    { name: 'reskin', path: 'landfills/reskin' },
+    { name: 'wa', path: 'landfills/wa' },
+  ])
+  .setAliasAdUnits('maintenance', [
+    { name: 'lb1', templateName: 'LB', path: 'maintenance/lb1' },
+    { name: 'lb2', templateName: 'LB2', path: 'maintenance/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'maintenance/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'maintenance/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'maintenance/load-more' },
+    { name: 'reskin', path: 'maintenance/reskin' },
+    { name: 'wa', path: 'maintenance/wa' },
+  ])
+  .setAliasAdUnits('recycling', [
+    { name: 'lb1', templateName: 'LB', path: 'recycling/lb1' },
+    { name: 'lb2', templateName: 'LB2', path: 'recycling/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'recycling/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'recycling/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'recycling/load-more' },
+    { name: 'reskin', path: 'recycling/reskin' },
+    { name: 'wa', path: 'recycling/wa' },
+  ])
+  .setAliasAdUnits('technology', [
+    { name: 'lb1', templateName: 'LB', path: 'technology/lb1' },
+    { name: 'lb2', templateName: 'LB2', path: 'technology/lb2' },
+    { name: 'rail1', templateName: 'CONTENT', path: 'technology/rail1' },
+    { name: 'rail2', templateName: 'CONTENT', path: 'technology/rail2' },
+    { name: 'load-more', templateName: 'CONTENT', path: 'technology/load-more' },
+    { name: 'reskin', path: 'technology/reskin' },
+    { name: 'wa', path: 'technology/wa' },
   ]);
 
 module.exports = config;
