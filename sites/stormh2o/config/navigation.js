7b2e699bf1fa5f7a74fae346c9f9761f821c3b1d
Brian Miller <bmiller@southcomm.com>
2019-10-22 20:50:06 -0500

    clone stw from frn

---
+++ sites/stormh2o/config/navigation.js
@@ -0,0 +1,72 @@
+module.exports = {
+  primary: {
+    items: [
+      { href: '/distributed-energy', label: 'Distributed Energy' },
+      { href: '/erosion-control', label: 'Erosion Control' },
+      { href: '/grading-excavation-contractor', label: 'Grading & Excavation Contractor' },
+      { href: '/msw-management', label: 'MSW Management' },
+      { href: '/stormwater', label: 'Stormwater' },
+      { href: '/water-efficiency', label: 'Water Efficiency' },
+    ],
+  },
+  secondary: {
+    items: [
+      { href: 'https://www.stormcon.com', label: 'Stormcon', target: '_blank' },
+      { href: '/free-reports', label: 'Free Reports' },
+      { href: '/page/advertise', label: 'Advertise' },
+      { href: 'https://www.foresteruniversity.com/ProductSubCats.aspx?SubCatID=47', label: 'Books', target: '_blank' },
+      { href: 'https://www.foresteruniversity.com', label: 'Training', target: '_blank' },
+      { href: '/subscribe', label: 'Subscribe' },
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/page/about-us', label: 'About Us' },
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: '/page/advertise', label: 'Advertise' },
+      { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+      { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+        { href: '/distributed-energy', label: 'Distributed Energy' },
+        { href: '/erosion-control', label: 'Erosion Control' },
+        { href: '/grading-excavation-contractor', label: 'Grading & Excavation Contractor' },
+        { href: '/msw-management', label: 'MSW Management' },
+        { href: '/stormwater', label: 'Stormwater' },
+        { href: '/water-efficiency', label: 'Water Efficiency' },
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+        { href: 'https://www.stormcon.com', label: 'Stormcon', target: '_blank' },
+        { href: '/free-reports', label: 'Free Reports' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: 'https://www.foresteruniversity.com/ProductSubCats.aspx?SubCatID=47', label: 'Books', target: '_blank' },
+        { href: 'https://www.foresteruniversity.com', label: 'Training', target: '_blank' },
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/events', label: 'Events' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: '/page/about-us', label: 'About Us' },
+        { href: '/contact-us', label: 'Contact Us' },
+        { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+        { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
+      ],
+    },
+  ],
+};
