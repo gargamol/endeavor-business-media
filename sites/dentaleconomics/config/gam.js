d1ede0bf1d9cec161e0da840a6e5d7ae3b0eb76b
Brian Miller <bmiller@southcomm.com>
2019-10-23 16:17:22 -0500

    revert de changes

--- sites/dentaleconomics/config/gam.js
+++ sites/dentaleconomics/config/gam.js
@@ -1,6 +1,6 @@
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'DTE' });
+const config = new GAMConfiguration('21687441225', { basePath: 'DE' });
 
 config
   .setTemplate('LB', {
