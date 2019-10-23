1c124d912cf1526d3d801360ba84d5fd2afeb4f3
Brian Miller <bmiller@southcomm.com>
2019-10-22 21:57:26 -0500

    fix image path

--- sites/gxcontractor/config/site.js
+++ sites/gxcontractor/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.gradingandexcavating.com/files/base/ebm/frn/image/static/logo/site_logo.png?h=45',
+      src: 'https://img.gradingandexcavating.com/files/base/ebm/gxc/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://img.gradingandexcavating.com/files/base/ebm/frn/image/static/logo/site_logo.png?h=90 2x',
+        'https://img.gradingandexcavating.com/files/base/ebm/gxc/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://img.gradingandexcavating.com/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      src: 'https://img.gradingandexcavating.com/files/base/ebm/gxc/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://img.gradingandexcavating.com/files/base/ebm/frn/image/static/logo/site_logo.png?h=120 2x',
+        'https://img.gradingandexcavating.com/files/base/ebm/gxc/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
@@ -30,7 +30,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://img.gradingandexcavating.com/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      logo: 'https://img.gradingandexcavating.com/files/base/ebm/gxc/image/static/logo/site_logo.png?h=60',
     },
     to: 'adsales@endeavorb2b.com',
   },
