4e6ad33ca1fd769835b0fe6ef471747472ca6ae5
Brian Miller <bmiller@southcomm.com>
2019-10-22 20:57:48 -0500

    update image reference in site config

--- sites/stormh2o/config/site.js
+++ sites/stormh2o/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=45',
+      src: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=90 2x',
+        'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      src: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=120 2x',
+        'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
@@ -34,7 +34,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      logo: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
     },
     to: 'adsales@endeavorb2b.com',
   },
