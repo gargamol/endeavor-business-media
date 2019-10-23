506dd570b1529bcc5736d01991a9069f5d952da1
Brian Miller <bmiller@southcomm.com>
2019-10-22 19:13:51 -0500

    update images and removed socialMedia Refs

--- sites/distributedenergy/config/site.js
+++ sites/distributedenergy/config/site.js
@@ -5,23 +5,19 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=45',
+      src: 'https://img.distributedenergy.com/files/base/ebm/dte/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=90 2x',
+        'https://img.distributedenergy.com/files/base/ebm/dte/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      src: 'https://img.distributedenergy.com/files/base/ebm/dte/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=120 2x',
+        'https://img.distributedenergy.com/files/base/ebm/dte/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
-  socialMediaLinks: [
-    { provider: 'linkedin', href: 'https://www.linkedin.com/company/forester_media_inc-/' },
-    { provider: 'twitter', href: 'https://twitter.com/foresternetwork' },
-    { provider: 'facebook', href: 'https://www.facebook.com/foresternetwork' },
-  ],
+  socialMediaLinks: [],
   gtm: {
     containerId: 'GTM-TNFLPD9',
   },
@@ -34,7 +30,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://base.imgix.net/files/base/ebm/frn/image/static/logo/site_logo.png?h=60',
+      logo: 'https://img.distributedenergy.com/files/base/ebm/dte/image/static/logo/site_logo.png?h=60',
     },
     to: 'adsales@endeavorb2b.com',
   },
