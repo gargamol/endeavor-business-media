0319085181ae399fcc481b46aa6302c70268d4fe
Brian Miller <bmiller@southcomm.com>
2019-10-23 08:56:24 -0500

    update native-x ads

--- sites/stormh2o/config/native-x.js
+++ sites/stormh2o/config/native-x.js
@@ -4,31 +4,22 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'default', id: '5d9cee75cc0836000147f154' },
+    { name: 'default', id: '5db05b30c99ad20001f14492' },
   ])
-  .setAliasPlacements('instrumentation', [
-    { name: 'default', id: '5d9ceeafcc08360001480602' },
+  .setAliasPlacements('bmps', [
+    { name: 'default', id: '5db05b75c99ad20001f162f4' },
   ])
-  .setAliasPlacements('valves-actuators', [
-    { name: 'default', id: '5d9cef2806c2650001d0ffb1' },
+  .setAliasPlacements('green-infrastructure', [
+    { name: 'default', id: '5db05b653204b10001f9a969' },
   ])
-  .setAliasPlacements('bearings-seals', [
-    { name: 'default', id: '5d9cef59cc08360001484adf' },
+  .setAliasPlacements('program-management', [
+    { name: 'default', id: '5db05b58c99ad20001f15628' },
   ])
-  .setAliasPlacements('piping-tubing-hosing', [
-    { name: 'default', id: '5d9cf0a2cc0836000148d170' },
+  .setAliasPlacements('compliance', [
+    { name: 'default', id: '5db05b4e3204b10001f99f2a' },
   ])
-  .setAliasPlacements('process-control-automation', [
-    { name: 'default', id: '5d9cf22dcc08360001496372' },
-  ])
-  .setAliasPlacements('pumps-motors-drives', [
-    { name: 'default', id: '5d9cf27c06c2650001d24aed' },
-  ])
-  .setAliasPlacements('maintenance-safety', [
-    { name: 'default', id: '5d9cf2a706c2650001d259f0' },
-  ])
-  .setAliasPlacements('industry-applications', [
-    { name: 'default', id: '5d9c8124cc08360001194f5a' },
+  .setAliasPlacements('erosion-control', [
+    { name: 'default', id: '5db05b3fc99ad20001f14a8f' },
   ]);
 
 module.exports = config;
