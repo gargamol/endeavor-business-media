529bc17831c12a9b1e687ca361779425f018fad8
Brian Miller <bmiller@southcomm.com>
2019-10-22 20:28:08 -0500

    Add GCSE config

--- sites/mswmanagement/config/gcse.js
+++ sites/mswmanagement/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('003355913687346718228:pszdjtovyyr');
+const config = new GCSEConfiguration('003355913687346718228:pu4uhywoqoi');
 
 module.exports = config;
