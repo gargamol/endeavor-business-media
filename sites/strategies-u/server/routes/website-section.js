a1ab0ef9557a47899470e0706ed86e00a55db6de
Alysha Buselt <>
2019-10-29 10:57:34 -0500

    CS3413-SU-whitepaper
    Conversion of white-papers from published content to a website section
    
    https://southcomm.atlassian.net/projects/CS/queues/custom/11/CS-3413

--- sites/strategies-u/server/routes/website-section.js
+++ sites/strategies-u/server/routes/website-section.js
@@ -3,6 +3,7 @@ const directory = require('../templates/website-section/directory');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
 const queryFragment = require('../graphql/fragments/website-section-page');
+const whitePapers = require('../templates/website-section/white-papers');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
@@ -15,6 +16,10 @@ module.exports = (app) => {
   }));
   app.get('/:alias(directory/[a-z0-9-/]+)', withWebsiteSection({
     template: directory,
+    queryFragment,
+  }));
+  app.get('/:alias(white-papers)', withWebsiteSection({
+    template: whitePapers,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
