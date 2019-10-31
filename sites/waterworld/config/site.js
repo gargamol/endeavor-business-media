9263d36e7bdc3f94df4ff37b7bb8a415337711c6
Brian Miller <bmiller@southcomm.com>
2019-10-31 18:19:05 -0500

    update image domain

--- sites/waterworld/config/site.js
+++ sites/waterworld/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=45',
+      src: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=90 2x',
+        'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
+      src: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=120 2x',
+        'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
@@ -23,7 +23,7 @@ module.exports = {
     { provider: 'facebook', href: 'https://www.facebook.com/foresternetwork' },
   ],
   gtm: {
-    containerId: 'GTM-TLSVP6Q',
+    containerId: 'GTM-MXBGH3V',
   },
   wufoo: {
     userName: 'cygnuscorporate',
@@ -34,7 +34,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://img.stormh2o.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
+      logo: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
     },
     to: 'adsales@endeavorb2b.com',
   },
