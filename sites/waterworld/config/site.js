a1a65d7a7faa607fc2d55be8de6a7056fd8bc24f
Brian Miller <bmiller@southcomm.com>
2019-10-31 18:41:19 -0500

    css and logos

--- sites/waterworld/config/site.js
+++ sites/waterworld/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=45',
+      src: 'https://img.waterworld.com/files/base/ebm/ww/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=90 2x',
+        'https://img.waterworld.com/files/base/ebm/ww/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
+      src: 'https://img.waterworld.com/files/base/ebm/ww/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=120 2x',
+        'https://img.waterworld.com/files/base/ebm/ww/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
@@ -34,7 +34,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://img.waterworld.com/files/base/ebm/stw/image/static/logo/site_logo.png?h=60',
+      logo: 'https://img.waterworld.com/files/base/ebm/ww/image/static/logo/site_logo.png?h=60',
     },
     to: 'adsales@endeavorb2b.com',
   },
