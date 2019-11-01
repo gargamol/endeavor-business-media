7cd5421537afcf73bc8b22658ec49544b042cccb
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-01 14:50:03 -0500

    Added technologies route & layout

--- sites/waterworld/server/routes/website-section.js
+++ sites/waterworld/server/routes/website-section.js
@@ -1,11 +1,16 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
+const technologiesTemplate = require('../templates/website-section/technologies');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
+    queryFragment,
+  }));
+  app.get('/:alias(technologies)', withWebsiteSection({
+    template: technologiesTemplate,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
