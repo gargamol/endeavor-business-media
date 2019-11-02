626252f68a1cd87599e7deb0ef53a290e36d9b50
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-02 08:54:07 -0500

    Initial commit for GTL

--- sites/waterworld/server/routes/website-section.js
+++ sites/waterworld/server/routes/website-section.js
@@ -2,6 +2,7 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
 const technologiesTemplate = require('../templates/website-section/technologies');
+const globalThoughtLeadersTemplate = require('../templates/website-section/global-thought-leaders');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
@@ -11,6 +12,10 @@ module.exports = (app) => {
   }));
   app.get('/:alias(technologies)', withWebsiteSection({
     template: technologiesTemplate,
+    queryFragment,
+  }));
+  app.get('/:alias(global-thought-leaders)', withWebsiteSection({
+    template: globalThoughtLeadersTemplate,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
