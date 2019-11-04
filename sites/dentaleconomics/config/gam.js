b8053f35a35198a027a3b74facae8994da149ff6
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-04 15:27:30 -0600

    Dropped 300x600 in favor of 300x250 for RDH, DIQ, DE

--- sites/dentaleconomics/config/gam.js
+++ sites/dentaleconomics/config/gam.js
@@ -12,8 +12,8 @@ config
     ],
   })
   .setTemplate('RAIL1', { size: [300, 250] })
-  .setTemplate('RAIL2', { size: [300, 600] })
-  .setTemplate('LM', { size: [[300, 250], [300, 600]] });
+  .setTemplate('RAIL2', { size: [300, 250] })
+  .setTemplate('LM', { size: [300, 250] });
 
 config
   .setAliasAdUnits('default', [
