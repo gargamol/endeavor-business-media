490709baf0995e4752afab8cf64cb2ed1491c0dd
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-05 07:13:26 -0600

    Fix wastewater LB template

--- sites/watertechonline/config/gam.js
+++ sites/watertechonline/config/gam.js
@@ -40,7 +40,7 @@ config
     { name: 'reskin', path: 'process-water/reskin' },
   ])
   .setAliasAdUnits('wastewater', [
-    { name: 'lb1', templateName: 'LB1', path: 'wastewater/lb1' },
+    { name: 'lb1', templateName: 'LB', path: 'wastewater/lb1' },
     { name: 'lb2', templateName: 'LB2', path: 'wastewater/lb2' },
     { name: 'rail1', templateName: 'CONTENT', path: 'wastewater/rail1' },
     { name: 'rail2', templateName: 'CONTENT', path: 'wastewater/rail2' },
