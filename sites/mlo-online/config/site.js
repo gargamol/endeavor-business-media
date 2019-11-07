60b9813fa8847cc7fb0812af9761654a73436b75
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2019-11-07 11:00:10 -0600

    site.js (MLO)
    I set the bcc email address to eactivity@endeavorb2b.com, which is an alias of emailactivity@cygnus.com, so technically both will work anyway.

--- sites/mlo-online/config/site.js
+++ sites/mlo-online/config/site.js
@@ -5,46 +5,46 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=45',
+      src: 'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=45',
       srcset: [
-        'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=90 2x',
+        'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=60',
+      src: 'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=60',
       srcset: [
-        'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=120 2x',
+        'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=120 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/2896249/', target: '_blank' },
-    { provider: 'twitter', href: 'https://twitter.com/LaserFocusWorld', target: '_blank' },
-    { provider: 'facebook', href: 'https://www.facebook.com/pages/Laser-Focus-World/126899915297', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/?home=&gid=2301731&trk=groups_guest_about-h-logo', target: '_blank' },
+    { provider: 'twitter', href: 'https://twitter.com/MedicalLabMLO', target: '_blank' },
+    { provider: 'facebook', href: 'https://www.facebook.com/MLOMedicalLaboratoryObserver', target: '_blank' },
   ],
   gtm: {
-    containerId: 'GTM-MFCT2LV',
+    containerId: 'GTM-PR8N96M',
   },
   wufoo: {
     userName: 'cygnuscorporate',
   },
   magazines: {
-    description: '',
+    description: 'mlo-online.com features online exclusive content as well as feature articles and stories from our print publications. Access digital editions and recent issues for our magazine here.',
   },
   contactUs: {
     branding: {
-      bgColor: '#164f77',
-      logo: 'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=60',
+      bgColor: '#000000',
+      logo: 'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=60',
     },
-    to: 'juliac@pennwell.com',
+    to: 'krussell@mlo-online.com',
   },
   inquiry: {
     enabled: true,
     directSend: true,
-    sendTo: 'bgweb@endeavorb2b.com',
-    sendFrom: 'LaserFocusWorld.com <noreply@baseplatform.io>',
-    sendBcc: 'emailactivity@cygnus.com',
-    logo: 'https://img.laserfocusworld.com/files/base/pennwell/lfw/logo.png?h=60',
-    bgColor: '#164f77',
+    sendTo: 'krussell@mlo-online.com',
+    sendFrom: 'Medical Laboratory Observer <noreply@baseplatform.io>',
+    sendBcc: 'eactivity@endeavorb2b.com',
+    logo: 'https://img.mlo-online.com/files/base/ebm/mlo/image/static/logo/site_logo.png?h=60',
+    bgColor: '#000000',
   },
 };
