b13bdaaaad297acfd8cdbb70ba425feccb35d108
Brian Miller <bmiller@southcomm.com>
2019-11-07 13:13:34 -0600

    best guess at ads :(

--- sites/officer/config/gam.js
+++ sites/officer/config/gam.js
@@ -1,6 +1,6 @@
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'LFW' });
+const config = new GAMConfiguration('21687441225', { basePath: 'Officer' });
 
 config
   .setTemplate('LB', {
@@ -17,61 +17,13 @@ config
 
 config
   .setAliasAdUnits('default', [
-    { name: 'lb1', templateName: 'LB', path: 'default/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'default/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'default/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'default/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'default/load-more' },
-    { name: 'reskin', path: 'default/reskin' },
-    { name: 'wa', path: 'default/wa' },
-  ])
-  .setAliasAdUnits('detectors-imaging', [
-    { name: 'lb1', templateName: 'LB', path: 'detectors-imaging/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'detectors-imaging/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'detectors-imaging/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'detectors-imaging/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'detectors-imaging/load-more' },
-    { name: 'reskin', path: 'detectors-imaging/reskin' },
-  ])
-  .setAliasAdUnits('lasers-sources', [
-    { name: 'lb1', templateName: 'LB', path: 'lasers-sources/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'lasers-sources/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'lasers-sources/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'lasers-sources/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'lasers-sources/load-more' },
-    { name: 'reskin', path: 'lasers-sources/reskin' },
-  ])
-  .setAliasAdUnits('optics', [
-    { name: 'lb1', templateName: 'LB', path: 'optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'optics/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'optics/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'optics/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'optics/load-more' },
-    { name: 'reskin', path: 'optics/reskin' },
-  ])
-  .setAliasAdUnits('fiber-optics', [
-    { name: 'lb1', templateName: 'LB', path: 'fiber-optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'fiber-optics/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'fiber-optics/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'fiber-optics/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'fiber-optics/load-more' },
-    { name: 'reskin', path: 'fiber-optics/reskin' },
-  ])
-  .setAliasAdUnits('software-accessories', [
-    { name: 'lb1', templateName: 'LB', path: 'software-accessories/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'software-accessories/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'software-accessories/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'software-accessories/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'software-accessories/load-more' },
-    { name: 'reskin', path: 'software-accessories/reskin' },
-  ])
-  .setAliasAdUnits('test-measurement', [
-    { name: 'lb1', templateName: 'LB', path: 'test-measurement/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'test-measurement/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'test-measurement/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'test-measurement/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'test-measurement/load-more' },
-    { name: 'reskin', path: 'test-measurement/reskin' },
+    { name: 'lb1', templateName: 'LB', path: 'Officer_BS' },
+    { name: 'lb2', templateName: 'LB', path: 'Officer_LB' },
+    { name: 'rail1', templateName: 'RAIL1', path: 'Officer_MR' },
+    { name: 'rail2', templateName: 'RAIL2', path: 'Officer_HP' },
+    { name: 'load-more', templateName: 'LM', path: 'Officer_FL' },
+    { name: 'reskin', path: 'Officer_Reskin' },
+    { name: 'wa', path: 'Officer_WA' },
   ]);
 
 module.exports = config;
