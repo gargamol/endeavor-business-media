a1fe7c9c1aa9156abb739bfc09e8f29938d8ccb1
Brian Miller <bmiller@southcomm.com>
2019-11-07 09:52:52 -0600

    udpate GAM

--- sites/vendingmarketwatch/config/gam.js
+++ sites/vendingmarketwatch/config/gam.js
@@ -1,6 +1,6 @@
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'LFW' });
+const config = new GAMConfiguration('21687441225', { basePath: 'VMW' });
 
 config
   .setTemplate('LB', {
@@ -24,54 +24,6 @@ config
     { name: 'load-more', templateName: 'LM', path: 'default/load-more' },
     { name: 'reskin', path: 'default/reskin' },
     { name: 'wa', path: 'default/wa' },
-  ])
-  .setAliasAdUnits('detectors-imaging', [
-    { name: 'lb1', templateName: 'LB', path: 'detectors-imaging/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'detectors-imaging/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'detectors-imaging/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'detectors-imaging/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'detectors-imaging/load-more' },
-    { name: 'reskin', path: 'detectors-imaging/reskin' },
-  ])
-  .setAliasAdUnits('lasers-sources', [
-    { name: 'lb1', templateName: 'LB', path: 'lasers-sources/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'lasers-sources/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'lasers-sources/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'lasers-sources/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'lasers-sources/load-more' },
-    { name: 'reskin', path: 'lasers-sources/reskin' },
-  ])
-  .setAliasAdUnits('optics', [
-    { name: 'lb1', templateName: 'LB', path: 'optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'optics/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'optics/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'optics/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'optics/load-more' },
-    { name: 'reskin', path: 'optics/reskin' },
-  ])
-  .setAliasAdUnits('fiber-optics', [
-    { name: 'lb1', templateName: 'LB', path: 'fiber-optics/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'fiber-optics/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'fiber-optics/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'fiber-optics/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'fiber-optics/load-more' },
-    { name: 'reskin', path: 'fiber-optics/reskin' },
-  ])
-  .setAliasAdUnits('software-accessories', [
-    { name: 'lb1', templateName: 'LB', path: 'software-accessories/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'software-accessories/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'software-accessories/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'software-accessories/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'software-accessories/load-more' },
-    { name: 'reskin', path: 'software-accessories/reskin' },
-  ])
-  .setAliasAdUnits('test-measurement', [
-    { name: 'lb1', templateName: 'LB', path: 'test-measurement/lb1' },
-    { name: 'lb2', templateName: 'LB', path: 'test-measurement/lb2' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'test-measurement/rail1' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'test-measurement/rail2' },
-    { name: 'load-more', templateName: 'LM', path: 'test-measurement/load-more' },
-    { name: 'reskin', path: 'test-measurement/reskin' },
   ]);
 
 module.exports = config;
