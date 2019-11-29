7704a6783800950b3b607dbbef1e9089f39b953c
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-29 11:30:19 -0600

    Update webinars route

--- sites/hcinnovationgroup/server/routes/published-content.js
+++ sites/hcinnovationgroup/server/routes/published-content.js
@@ -4,6 +4,6 @@ const videos = require('../templates/published-content/videos');
 
 module.exports = (app) => {
   app.get('/events', (_, res) => { res.marko(events); });
-  app.get('/webcasts', (_, res) => { res.marko(webinars); });
+  app.get('/webinars', (_, res) => { res.marko(webinars); });
   app.get('/videos', (_, res) => { res.marko(videos); });
 };
