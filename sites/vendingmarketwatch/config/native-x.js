9b2e15cb8909605315559b0761a005cb4c247c1e
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-28 18:06:56 -0600

    Added native-x config

--- sites/vendingmarketwatch/config/native-x.js
+++ sites/vendingmarketwatch/config/native-x.js
@@ -4,7 +4,31 @@ const config = new NativeXConfiguration('https://ebm.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'default', id: '@todoCreateMe' },
+    { name: 'default', id: '5c0e865fcf399b00017aeae9' },
+  ])
+  .setAliasPlacements('beverage', [
+    { name: 'default', id: '5c0e8768cf399b00017afed7' },
+  ])
+  .setAliasPlacements('coffee-service', [
+    { name: 'default', id: '5c0e874acf399b00017afcd7' },
+  ])
+  .setAliasPlacements('equipment', [
+    { name: 'default', id: '5c0e872d84c2cd000148d879' },
+  ])
+  .setAliasPlacements('healthy-convenience', [
+    { name: 'default', id: '5c0e870484c2cd000148d600' },
+  ])
+  .setAliasPlacements('management', [
+    { name: 'default', id: '5c0e86e2cf399b00017af5bc' },
+  ])
+  .setAliasPlacements('micro-market', [
+    { name: 'default', id: '5c0e86c484c2cd000148d0f2' },
+  ])
+  .setAliasPlacements('products', [
+    { name: 'default', id: '5c0e8699cf399b00017aeeca' },
+  ])
+  .setAliasPlacements('technology', [
+    { name: 'default', id: '5c0e867684c2cd000148ca23' },
   ]);
 
 module.exports = config;
