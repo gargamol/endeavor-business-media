ce5a97b23c7063f1d868e484618c3a30e25d97ce
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-01 11:19:28 -0600

    Update routes

--- sites/hpnonline/server/routes/published-content.js
+++ sites/hpnonline/server/routes/published-content.js
@@ -1,11 +1,9 @@
 const events = require('../templates/published-content/events');
-const whitepapers = require('../templates/published-content/whitepapers');
 const webinars = require('../templates/published-content/webinars');
 const videos = require('../templates/published-content/videos');
 
 module.exports = (app) => {
   app.get('/events', (_, res) => { res.marko(events); });
-  app.get('/whitepapers', (_, res) => { res.marko(whitepapers); });
   app.get('/webinars', (_, res) => { res.marko(webinars); });
   app.get('/videos', (_, res) => { res.marko(videos); });
 };
