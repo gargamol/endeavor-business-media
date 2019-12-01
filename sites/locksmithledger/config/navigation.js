41d0260ff8e0389710ebd44c364fd3b7f8e4aa29
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-01 17:34:54 -0600

    Update navigation

--- sites/locksmithledger/config/navigation.js
+++ sites/locksmithledger/config/navigation.js
@@ -1,22 +1,21 @@
 module.exports = {
   primary: {
     items: [
-      { href: '/security-executives', label: 'Security Executives' },
-      { href: '/integrators', label: 'Integrators' },
-      { href: '/video-surveillance', label: 'Video Surveillance' },
-      { href: '/access-identity', label: 'Access Identity' },
-      { href: '/residential-technologies', label: 'Residential Tech' },
-      { href: '/alarms-monitoring', label: 'Alarms & Monitoring' },
-      { href: '/cybersecurity', label: 'Cybersecurity' },
-      { href: '/perimeter-security', label: 'Perimeter' },
+      { href: '/alarms-video', label: 'Alarms & Video' },
+      { href: '/door-hardware', label: 'Door Hardware' },
+      { href: '/keys-tools', label: 'Keys Tools' },
+      { href: '/locks', label: 'Locks' },
+      { href: '/electronics-access-control', label: 'Electronics & Access Control' },
+      { href: '/safes', label: 'Safes' },
+      { href: '/distributors', label: 'Distributors' },
     ],
   },
   secondary: {
     items: [
-      { href: 'https://forums.securityinfowatch.com', label: 'Forums', target: '_blank' },
+      { href: '/codesexpress', label: 'CodesExpress' },
+      { href: 'https://forums.locksmithledger.com', label: 'Forums', target: '_blank' },
       { href: '/directory', label: 'Buyer\'s Guide' },
-      { href: '/subscribe', label: 'Subscribe' },
-      { href: '/advertise', label: 'Advertise' },
+      { href: '/contact-us', label: 'Contact Us' },
     ],
   },
   tertiary: {
@@ -26,7 +25,6 @@ module.exports = {
   },
   footer: {
     items: [
-      { href: '/page/about-us', label: 'About Us' },
       { href: '/contact-us', label: 'Contact Us' },
       { href: '/page/advertise', label: 'Advertise' },
       { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
@@ -37,39 +35,24 @@ module.exports = {
     {
       label: 'Topics',
       items: [
-        { href: '/security-executives', label: 'Security Executives' },
-        { href: '/integrators', label: 'Integrators' },
-        { href: '/video-surveillance', label: 'Video Surveillance' },
-        { href: '/access-identity', label: 'Access Identity' },
-        { href: '/residential-technologies', label: 'Residential Technologies' },
-        { href: '/alarms-monitoring', label: 'Alarms & Monitoring' },
-        { href: '/cybersecurity', label: 'Cybersecurity' },
-        { href: '/perimeter-security', label: 'Perimeter Security' },
+        { href: '/alarms-video', label: 'Alarms & Video' },
+        { href: '/door-hardware', label: 'Door Hardware' },
+        { href: '/keys-tools', label: 'Keys Tools' },
+        { href: '/locks', label: 'Locks' },
+        { href: '/electronics-access-control', label: 'Electronics & Access Control' },
+        { href: '/safes', label: 'Safes' },
+        { href: '/distributors', label: 'Distributors' },
       ],
     },
     {
       label: 'Resources',
       items: [
-        { href: 'https://forums.securityinfowatch.com', label: 'Forums', target: '_blank' },
+        { href: '/codesexpress', label: 'CodesExpress' },
+        { href: 'https://forums.locksmithledger.com', label: 'Forums', target: '_blank' },
         { href: '/directory', label: 'Buyer\'s Guide' },
-        { href: '/government', label: 'Government' },
-        { href: '/critical-infrastructure', label: 'Critical Infrastructure' },
-        { href: '/retail', label: 'Retail' },
-        { href: '/education', label: 'Education' },
-        { href: '/healthcare', label: 'Healthcare' },
-        { href: '/security-solutions-corner', label: 'Security Solutions Corner' },
-        { href: '/white-papers', label: 'Whitepapers' },
-        { href: '/webinars', label: 'Webinars' },
-      ],
-    },
-    {
-      label: 'Events',
-      items: [
-        { href: '/secured-cities', label: 'Secured Cities' },
-        { href: '/11713862', label: 'Fast50' },
-        { href: '/isc-west', label: 'ISC West' },
-        { href: '/gsx', label: 'GSX' },
-        { href: '/ces', label: 'CES' },
+        { href: '/events', label: 'Events' },
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/classifieds', label: 'Classifieds' },
       ],
     },
     {
@@ -77,7 +60,6 @@ module.exports = {
       items: [
         { href: '/subscribe', label: 'Subscribe' },
         { href: '/page/advertise', label: 'Advertise' },
-        { href: '/page/about-us', label: 'About Us' },
         { href: '/contact-us', label: 'Contact Us' },
         { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
         { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
