b2bcced9b1a7aaf6744f83c770818f999ea1d606
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-02 11:46:59 -0600

    Update gcse config

--- sites/vehicleservicepros/config/gcse.js
+++ sites/vehicleservicepros/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('017383739850048358259:mzffwbn1gsa');
+const config = new GCSEConfiguration('017383739850048358259:o1acwmn9wua');
 
 module.exports = config;
