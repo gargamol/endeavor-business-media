fccf01601fb92ce83e7738564f301d5dfac83806
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-07 15:26:45 -0600

    Update gcse config

--- sites/cpapracticeadvisor/config/gcse.js
+++ sites/cpapracticeadvisor/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('017383739850048358259:mzffwbn1gsa');
+const config = new GCSEConfiguration('017383739850048358259:fdbdgb9t5h0');
 
 module.exports = config;
