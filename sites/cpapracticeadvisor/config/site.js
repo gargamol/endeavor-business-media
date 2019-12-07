dbb55179920d38e73d21f072eaecd9ceac80b339
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-07 15:26:50 -0600

    Update site config

--- sites/cpapracticeadvisor/config/site.js
+++ sites/cpapracticeadvisor/config/site.js
@@ -5,25 +5,25 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=60',
+      src: 'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=60',
       srcset: [
-        'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=120 2x',
+        'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=120 2x',
       ],
     },
     footer: {
-      src: 'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=60',
+      src: 'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=60',
       srcset: [
-        'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=120 2x',
+        'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=120 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'linkedin', href: 'http://www.linkedin.com/groups/8244648/profile', target: '_blank' },
-    { provider: 'twitter', href: 'http://www.twitter.com/SecInfoWatch', target: '_blank' },
-    { provider: 'facebook', href: 'http://www.facebook.com/SecInfoWatch', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/in/cpapracticeadvisor', target: '_blank' },
+    { provider: 'twitter', href: 'https://www.twitter.com/cpapracadvisor', target: '_blank' },
+    { provider: 'facebook', href: 'https://www.facebook.com/cpapracticeadvisor', target: '_blank' },
   ],
   gtm: {
-    containerId: 'GTM-KMZ7FHS',
+    containerId: 'GTM-WKFS2G3',
   },
   wufoo: {
     userName: 'cygnuscorporate',
@@ -34,17 +34,17 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=60',
+      logo: 'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=60',
     },
-    to: 'nbrokamp@endeavorb2b.com',
+    to: 'isaac.obannon@cpapracticeadvisor.com',
   },
   inquiry: {
     enabled: true,
     directSend: true,
-    sendTo: 'nbrokamp@endeavorb2b.com',
+    sendTo: 'bstrobel@southcomm.com',
     sendFrom: 'SecurityInfoWatch.com <noreply@baseplatform.io>',
     sendBcc: 'emailactivity@cygnus.com',
-    logo: 'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo_red_on_black.png?h=60',
+    logo: 'https://img.cpapracticeadvisor.com/files/base/cygnus/cpa/image/static/logo/site_logo_white.png?h=60',
     bgColor: '#164f77',
   },
 };
