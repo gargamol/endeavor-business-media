04dc7e9c2261b18b798367f76ea6f79032152e2c
Brian Miller <bmiller@southcomm.com>
2019-12-13 14:44:53 -0600

    update channels and section pages
     - add route overrides for channel pages to point at the hero, latest || ad || categories layout

--- sites/vendingmarketwatch.com/server/routes/website-section.js
+++ sites/vendingmarketwatch.com/server/routes/website-section.js
@@ -1,5 +1,6 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const directory = require('../templates/website-section/directory');
+const channel = require('../templates/website-section/channel');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
 const queryFragment = require('../graphql/fragments/website-section-page');
@@ -14,8 +15,36 @@ module.exports = (app) => {
     template: directory,
     queryFragment,
   }));
-  app.get('/:alias(directory/[a-z0-9-/]+)', withWebsiteSection({
-    template: directory,
+  app.get('/:alias(beverage)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(coffee-service)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(equipment)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(healthy-convenience)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(management)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(micro-market)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(products)', withWebsiteSection({
+    template: channel,
+    queryFragment,
+  }));
+  app.get('/:alias(technology)', withWebsiteSection({
+    template: channel,
     queryFragment,
   }));
   app.get('/:alias(white-papers)', withWebsiteSection({
