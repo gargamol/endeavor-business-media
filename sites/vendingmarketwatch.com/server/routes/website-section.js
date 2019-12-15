94a949851f234926712452eedf8d169acab5624c
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-15 13:12:36 -0600

    Add channelAliases to routing

--- sites/vendingmarketwatch.com/server/routes/website-section.js
+++ sites/vendingmarketwatch.com/server/routes/website-section.js
@@ -6,6 +6,17 @@ const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
 const whitePapers = require('../templates/website-section/white-papers');
 
+const channelAliases = [
+  'coffee-service',
+  'beverage',
+  'equipment',
+  'healthy-convenience',
+  'management',
+  'micro-market',
+  'products',
+  'technology',
+];
+
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
@@ -15,35 +26,7 @@ module.exports = (app) => {
     template: directory,
     queryFragment,
   }));
-  app.get('/:alias(beverage)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(coffee-service)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(equipment)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(healthy-convenience)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(management)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(micro-market)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(products)', withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
-  app.get('/:alias(technology)', withWebsiteSection({
+  app.get(`/:alias(${channelAliases.join('|')})`, withWebsiteSection({
     template: channel,
     queryFragment,
   }));
