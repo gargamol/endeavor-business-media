e705d8ff1651521a4015bfac5ab3ff5658a3a575
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-15 12:48:32 -0600

    Group template includes together

--- sites/vendingmarketwatch.com/server/routes/website-section.js
+++ sites/vendingmarketwatch.com/server/routes/website-section.js
@@ -1,9 +1,9 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
+const queryFragment = require('../graphql/fragments/website-section-page');
 const directory = require('../templates/website-section/directory');
 const channel = require('../templates/website-section/channel');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
-const queryFragment = require('../graphql/fragments/website-section-page');
 const whitePapers = require('../templates/website-section/white-papers');
 
 module.exports = (app) => {
