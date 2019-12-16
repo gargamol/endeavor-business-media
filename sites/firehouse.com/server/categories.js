17e05306f888b31325597abe6283e1fb59839fd7
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-15 19:57:38 -0600

    Add categories.js configs

--- sites/firehouse.com/server/categories.js
+++ sites/firehouse.com/server/categories.js
@@ -1,1 +1,549 @@
-module.exports = [];
+module.exports = [
+  {
+    id: 58039,
+    name: 'Apparatus Type',
+    href: '/apparatus/type',
+    values: [
+      {
+        id: 58040,
+        name: 'Aerial',
+        href: '/apparatus/type/aerial',
+      },
+      {
+        id: 58041,
+        name: 'All-Terrain',
+        href: '/apparatus/type/all-terrain',
+      },
+      {
+        id: 58042,
+        name: 'Alternative Response Vehicles',
+        href: '/apparatus/type/alternative-response-vehicles',
+      },
+      {
+        id: 58043,
+        name: 'Ambulance',
+        href: '/apparatus/type/ambulance',
+      },
+      {
+        id: 58044,
+        name: 'ARFF',
+        href: '/apparatus/type/arff',
+      },
+      {
+        id: 58045,
+        name: 'Boats',
+        href: '/apparatus/type/boats',
+      },
+      {
+        id: 58046,
+        name: 'Brush/Forestry',
+        href: '/apparatus/type/brush-forestry',
+      },
+      {
+        id: 58047,
+        name: 'Chief/Command',
+        href: '/apparatus/type/chief-command',
+      },
+      {
+        id: 58048,
+        name: 'Hazmat/Specialty',
+        href: '/apparatus/type/hazmat-specialty',
+      },
+      {
+        id: 58049,
+        name: 'Heavy Rescue',
+        href: '/apparatus/type/heavy-rescue',
+      },
+      {
+        id: 58050,
+        name: 'Pumper',
+        href: '/apparatus/type/pumper',
+      },
+      {
+        id: 58051,
+        name: 'Quint',
+        href: '/apparatus/type/quint',
+      },
+      {
+        id: 58052,
+        name: 'Tanker/Tender',
+        href: '/apparatus/type/tanker-tender',
+      },
+    ],
+  },
+  {
+    id: 58053,
+    name: 'Apparatus Components',
+    href: '/apparatus/components',
+    values: [
+      {
+        id: 58054,
+        name: 'Brakes & Suspension',
+        href: '/apparatus/components/brakes-suspension',
+      },
+      {
+        id: 58058,
+        name: 'Foam Systems',
+        href: '/apparatus/components/foam-systems',
+      },
+      {
+        id: 58055,
+        name: 'Electrical Components',
+        href: '/apparatus/components/electrical-components',
+      },
+      {
+        id: 58056,
+        name: 'Engines',
+        href: '/apparatus/components/engines',
+      },
+      {
+        id: 58057,
+        name: 'Equipment Mounting & Reels',
+        href: '/apparatus/components/equipment-mounting-reels',
+      },
+      {
+        id: 58058,
+        name: 'Foam Systems',
+        href: '/apparatus/components/foam-systems',
+      },
+      {
+        id: 58059,
+        name: 'Gauges & Instrumentation',
+        href: '/apparatus/components/gauges-instrumentation',
+      },
+      {
+        id: 58060,
+        name: 'Generators',
+        href: '/apparatus/components/generators',
+      },
+      {
+        id: 58061,
+        name: 'Governors',
+        href: '/apparatus/components/governors',
+      },
+      {
+        id: 58062,
+        name: 'Graphics',
+        href: '/apparatus/components/graphics',
+      },
+      {
+        id: 58063,
+        name: 'Mirrors & Vision Systems',
+        href: '/apparatus/components/mirrors-vision-systems',
+      },
+      {
+        id: 58064,
+        name: 'Pumps & Valves',
+        href: '/apparatus/components/pumps-valves',
+      },
+      {
+        id: 58065,
+        name: 'Seats',
+        href: '/apparatus/components/seats',
+      },
+      {
+        id: 58066,
+        name: 'Skid Units',
+        href: '/apparatus/components/skid-units',
+      },
+      {
+        id: 58068,
+        name: 'Vehicle Services & Inspections',
+        href: '/apparatus/components/vehicle-services-inspections',
+      },
+      {
+        id: 58067,
+        name: 'Warning Systems',
+        href: '/apparatus/components/warning-systems',
+      },
+    ],
+  },
+  {
+    id: 58111,
+    name: 'Architects',
+    href: '/stations/architects',
+  },
+  {
+    id: 58071,
+    name: 'Awards, Collectibles & Memorials',
+    href: '/careers-education/awards-collectibles-memorials',
+  },
+  {
+    id: 58112,
+    name: 'Building Components',
+    href: '/stations/building-components',
+    values: [
+      {
+        id: 58113,
+        name: 'Cleaning & Laundry',
+        href: '/stations/building-components/cleaning-laundry',
+      },
+      {
+        id: 58114,
+        name: 'Doors',
+        href: '/stations/building-components/doors',
+      },
+      {
+        id: 58115,
+        name: 'Exhaust Removal',
+        href: '/stations/building-components/exhaust-removal',
+      },
+      {
+        id: 58320,
+        name: 'Manufacturer\'s Corner',
+        href: '/manufacturers-corner',
+      },
+      {
+        id: 58116,
+        name: 'Flooring & Components',
+        href: '/stations/building-components/flooring-components',
+      },
+      {
+        id: 58117,
+        name: 'Furnishings',
+        href: '/stations/building-components/furnishings',
+      },
+      {
+        id: 58118,
+        name: 'In-Station Technology',
+        href: '/stations/building-components/in-station-technology',
+      },
+      {
+        id: 58119,
+        name: 'Kitchen & Accessories',
+        href: '/stations/building-components/kitchen-accessories',
+      },
+      {
+        id: 58120,
+        name: 'Racks & Lockers',
+        href: '/stations/building-components/racks-lockers',
+      },
+      {
+        id: 58121,
+        name: 'Security',
+        href: '/stations/building-components/security',
+      },
+    ],
+  },
+  {
+    id: 58072,
+    name: 'Colleges & Universities',
+    href: '/careers-education/colleges-universities',
+  },
+  {
+    id: 58123,
+    name: 'Communications',
+    href: '/tech-comm',
+    values: [
+      {
+        id: 58124,
+        name: 'CAD/Dispatch Systems',
+        href: '/tech-comm/cad-dispatch-systems',
+      },
+      {
+        id: 58125,
+        name: 'Mobile Technology & Accessories',
+        href: '/tech-comm/mobile-technology-accessories',
+      },
+      {
+        id: 58126,
+        name: 'Radios, Pagers & Accessories',
+        href: '/tech-comm/radios-pagers-accessories',
+      },
+    ],
+  },
+  {
+    id: 58081,
+    name: 'Community Risk Reduction',
+    href: '/prevention-investigation/community-risk-reduction',
+  },
+  {
+    id: null,
+    name: 'Company Submission Form',
+    href: '/company/submit',
+  },
+  {
+    id: 58073,
+    name: 'Emblems & Badges',
+    href: '/careers-education/emblems-badges',
+  },
+  {
+    id: 58083,
+    name: 'Fire Protection Systems',
+    href: '/prevention-investigation/fire-protection-systems',
+  },
+  {
+    id: 58085,
+    name: 'Hazardous Materials',
+    href: '/rescue/hazardous-materials',
+  },
+  {
+    id: 58093,
+    name: 'Health',
+    href: '/safety-health',
+    values: [
+      {
+        id: 58097,
+        name: 'Health & Fitness',
+        href: '/safety-health/health-fitness',
+      },
+      {
+        id: 58098,
+        name: 'Incident Rehab',
+        href: '/safety-health/incident-rehab',
+      },
+    ],
+  },
+  {
+    id: 58077,
+    name: 'Incident Command',
+    href: '/leadership/incident-command',
+  },
+  {
+    id: 58082,
+    name: 'Investigation Equipment',
+    href: '/prevention-investigation/investigation-equipment',
+  },
+  {
+    id: 58134,
+    name: 'Operations',
+    href: '/operations-training',
+    values: [
+      {
+        id: 58136,
+        name: 'Extinguishers',
+        href: '/operations-training/extinguishers',
+      },
+      {
+        id: 58137,
+        name: 'EMS',
+        href: '/operations-training/ems',
+      },
+      {
+        id: 58138,
+        name: 'Hoselines & Water Appliances',
+        href: '/operations-training/hoselines-water-appliances',
+      },
+      {
+        id: 58139,
+        name: 'Ladders',
+        href: '/operations-training/ladders',
+      },
+      {
+        id: 58140,
+        name: 'Patient Packaging',
+        href: '/operations-training/patient-packaging',
+      },
+      {
+        id: 58142,
+        name: 'Thermal Imagers',
+        href: '/operations-training/thermal-imagers',
+      },
+      {
+        id: 58143,
+        name: 'Tools',
+        href: '/operations-training/tools',
+      },
+      {
+        id: 58145,
+        name: 'Ventilation',
+        href: '/operations-training/ventilation',
+      },
+      {
+        id: 58146,
+        name: 'Wildland',
+        href: '/operations-training/wildland',
+      },
+    ],
+  },
+  {
+    id: 58099,
+    name: 'PPE',
+    href: '/safety-health/ppe',
+    values: [
+      {
+        id: 58096,
+        name: 'Chemical & Hazmat Gear',
+        href: '/safety-health/ppe/chemical-hazmat-gear',
+      },
+      {
+        id: 58100,
+        name: 'Eyewear',
+        href: '/safety-health/ppe/eyewear',
+      },
+      {
+        id: 58101,
+        name: 'Footwear',
+        href: '/safety-health/ppe/footwear',
+      },
+      {
+        id: 58102,
+        name: 'Gloves',
+        href: '/safety-health/ppe/gloves',
+      },
+      {
+        id: 58103,
+        name: 'Helmets',
+        href: '/safety-health/ppe/helmets',
+      },
+      {
+        id: 58104,
+        name: 'Hoods',
+        href: '/safety-health/ppe/hoods',
+      },
+      {
+        id: 58105,
+        name: 'Turnout Gear',
+        href: '/safety-health/ppe/turnout-gear',
+      },
+    ],
+  },
+  {
+    id: 58078,
+    name: 'Professional Associations & Services',
+    href: '/leadership/professional-associations-services',
+  },
+  {
+    id: null,
+    name: 'Product Submission Form',
+    href: '/product/submit',
+  },
+  {
+    id: 58086,
+    name: 'Rope',
+    href: '/rescue/rope',
+  },
+  {
+    id: 58093,
+    name: 'Safety',
+    href: '/safety-health',
+    values: [
+      {
+        id: 58094,
+        name: 'Accountability & Tracking',
+        href: '/safety-health/accountability-tracking',
+      },
+      {
+        id: 58095,
+        name: 'Bags & Backpacks',
+        href: '/safety-health/bags-backpacks',
+      },
+      {
+        id: 58107,
+        name: 'Scene Lighting',
+        href: '/safety-health/scene-lighting',
+      },
+      {
+        id: 58108,
+        name: 'Stationwear',
+        href: '/safety-health/stationwear',
+      },
+      {
+        id: 58109,
+        name: 'Uniforms',
+        href: '/safety-health/uniforms',
+      },
+    ],
+  },
+  {
+    id: 58087,
+    name: 'Technical Rescue',
+    href: '/rescue/technical-rescue',
+    values: [
+      {
+        id: 58088,
+        name: 'Confined Space',
+        href: '/rescue/technical-rescue/confined-space',
+      },
+      {
+        id: 58089,
+        name: 'Structural Collapse',
+        href: '/rescue/technical-rescue/structural-collapse',
+      },
+      {
+        id: 58090,
+        name: 'Trench Rescue',
+        href: '/rescue/technical-rescue/trench-rescue',
+      },
+    ],
+  },
+  {
+    id: 58123,
+    name: 'Technology',
+    href: '/tech-comm',
+    values: [
+      {
+        id: 58127,
+        name: 'Mapping & Pre-Fire Planning',
+        href: '/tech-comm/mapping-pre-fire-planning',
+      },
+      {
+        id: 58128,
+        name: 'Drones',
+        href: '/tech-comm/drones',
+      },
+      {
+        id: 58129,
+        name: 'Computers & Accessories',
+        href: '/tech-comm/computers-accessories',
+      },
+      {
+        id: 58130,
+        name: 'Atmospheric Monitoring',
+        href: '/tech-comm/atmospheric-monitoring',
+      },
+      {
+        id: 58131,
+        name: 'Billing & Inventory',
+        href: '/tech-comm/billing-inventory',
+      },
+      {
+        id: 58132,
+        name: 'Personnel Management Software',
+        href: '/tech-comm/personnel-management-software',
+      },
+      {
+        id: 58133,
+        name: 'Learning Management Systems',
+        href: '/tech-comm/learning-management-systems',
+      },
+    ],
+  },
+  {
+    id: 58134,
+    name: 'Training',
+    href: '/operations-training',
+    values: [
+      {
+        id: 58135,
+        name: 'Educational Materials',
+        href: '/operations-training/educational-materials',
+      },
+      {
+        id: 58141,
+        name: 'Props & Simulators',
+        href: '/operations-training/props-simulators',
+      },
+      {
+        id: 58144,
+        name: 'Structures',
+        href: '/operations-training/structures',
+      },
+      {
+        id: 58147,
+        name: 'Training Drills',
+        href: '/operations-training/training-drills',
+      },
+    ],
+  },
+  {
+    id: 58091,
+    name: 'Vehicle Extrication',
+    href: '/rescue/vehicle-extrication',
+  },
+  {
+    id: 58092,
+    name: 'Water & Ice',
+    href: '/rescue/water-ice',
+  },
+];
