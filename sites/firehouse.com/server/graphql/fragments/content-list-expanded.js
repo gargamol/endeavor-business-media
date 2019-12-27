d7fffe0b0092a8b945612c7e6933fa69eefb42ff
Brandon Krigbaum <brandonbk@gmail.com>
2019-12-26 18:57:48 -0600

    Add content-list-expanded gql fragment to include body field

---
+++ sites/firehouse.com/server/graphql/fragments/content-list-expanded.js
@@ -0,0 +1,66 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteContentListExpandedFragment on Content {
+  id
+  type
+  typeTitled: type(input: { format: titleize })
+  shortName
+  teaser(input: { maxLength: 500, useFallback: false, truncatedSuffix: "" })
+  body
+  canonicalPath
+  published
+  publishedDate(input: { format: "MMM Do, YYYY" })
+  company {
+    id
+    type
+    name
+    canonicalPath
+  }
+  primarySection {
+    id
+    name
+    fullName
+    canonicalPath
+  }
+  primaryImage {
+    id
+    src
+    alt
+    isLogo
+  }
+  userRegistration {
+    isRequired
+  }
+  ... on Authorable {
+    authors {
+      edges {
+        node {
+          id
+          name
+          type
+          canonicalPath
+        }
+      }
+    }
+  }
+  ... on ContentPromotion {
+    linkText
+    linkUrl
+  }
+  ... on ContentContact {
+    title
+    phone
+    publicEmail
+  }
+  ... on ContentWebinar {
+    starts
+  }
+  ... on ContentEvent {
+    starts
+    ends
+  }
+}
+
+`;
