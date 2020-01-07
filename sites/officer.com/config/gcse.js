5743562cb6809a602781967cedb8cef5d53e3016
Brian Miller <bmiller@southcomm.com>
2020-01-07 11:18:49 -0600

    fix OFCR & VMW search config

--- sites/officer.com/config/gcse.js
+++ sites/officer.com/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('003355913687346718228:p0nyaiybgnq');
+const config = new GCSEConfiguration('017383739850048358259:p0nyaiybgnq');
 
 module.exports = config;
--- sites/vendingmarketwatch.com/config/gcse.js
+++ sites/vendingmarketwatch.com/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('003355913687346718228:w2v5kwzqvrm');
+const config = new GCSEConfiguration('017383739850048358259:w2v5kwzqvrm');
 
 module.exports = config;
