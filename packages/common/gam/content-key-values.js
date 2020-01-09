f828498966343f1427dc5c5a5ba07f11ec216426
Jacob Bare <jacob@limit0.io>
2020-01-09 09:24:36 -0600

    Set comp_id to id when type is company

--- packages/common/gam/content-key-values.js
+++ packages/common/gam/content-key-values.js
@@ -1,7 +1,11 @@
 const { get } = require('@base-cms/object-path');
 
-module.exports = content => ({
-  cont_id: get(content, 'id'),
-  cont_type: get(content, 'type'),
-  comp_id: get(content, 'company.id'),
-});
+module.exports = (content) => {
+  const id = get(content, 'id');
+  const type = get(content, 'type');
+  return {
+    cont_id: id,
+    cont_type: type,
+    comp_id: type === 'company' ? id : get(content, 'company.id'),
+  };
+};
