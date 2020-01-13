4ca0de4be412b1ec300d38513c4e5290a52ad4aa
Josh Worden <solocommand@gmail.com>
2020-01-13 15:22:29 -0600

    Rename browser component

--- packages/leaders/browser/index.js
+++ packages/leaders/browser/index.js
@@ -1,9 +1,9 @@
 import Leaders from '@base-cms/marko-web-leaders/browser';
 
-const CommonGTMTrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './gtm-track-leaders.vue');
+const GTMTrackLeaders = () => import(/* webpackChunkName: "gtm-track-leaders" */ './gtm-track-leaders.vue');
 
 export default (Browser) => {
   const { EventBus } = Browser;
-  Browser.register('CommonGTMTrackLeaders', CommonGTMTrackLeaders, { provide: { EventBus } });
+  Browser.register('GTMTrackLeaders', GTMTrackLeaders, { provide: { EventBus } });
   Leaders(Browser);
 };
