73157bedbea9947308a8cc3f9fa3d2e23c75df5d
Josh Worden <solocommand@gmail.com>
2020-01-14 08:40:46 -0600

    Implement leaders for SIW

---
+++ sites/securityinfowatch.com/config/leaders.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Leadership in Security',
+  alias: 'leaders/2020',
+  header: {
+    imgSrc: 'https://img.securityinfowatch.com/files/base/cygnus/siw/image/static/logo/site_logo-white_on_black.png?h=90',
+  },
+};
