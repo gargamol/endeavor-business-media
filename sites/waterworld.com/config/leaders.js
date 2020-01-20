a1c63949e2e798be35788b41aa1e20d0bc777320
Brandon Krigbaum <brandonbk@gmail.com>
2020-01-20 09:40:12 -0600

    Add config support for leaders calllout value

--- sites/waterworld.com/config/leaders.js
+++ sites/waterworld.com/config/leaders.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Leaders in Water',
   alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
   header: {
     imgSrc: 'https://img.waterworld.com/files/base/ebm/ww/image/static/ww-leaders-logo.png?h=90',
   },
