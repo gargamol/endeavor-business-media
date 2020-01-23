aa672e69f5a11a1af627520998741cc9d1d3d7e2
Jacob Bare <jacob@limit0.io>
2020-01-23 10:57:30 -0600

    Rename component name to PhotoSwipe

--- packages/photoswipe/browser/index.js
+++ packages/photoswipe/browser/index.js
@@ -1,5 +1,5 @@
-const Photoswipe = () => import(/* webpackChunkName: "marko-web-photoswipe" */ './index.vue');
+const PhotoSwipe = () => import(/* webpackChunkName: "marko-web-photoswipe" */ './index.vue');
 
 export default (Browser) => {
-  Browser.register('Photoswipe', Photoswipe);
+  Browser.register('PhotoSwipe', PhotoSwipe);
 };
