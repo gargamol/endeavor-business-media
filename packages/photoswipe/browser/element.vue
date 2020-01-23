6dabb5de2a4f46bed78f4d79fcbba66682d61de2
Jacob Bare <jacob@limit0.io>
2020-01-23 10:58:24 -0600

    Use correct block name

--- packages/photoswipe/browser/element.vue
+++ packages/photoswipe/browser/element.vue
@@ -19,7 +19,7 @@ export default {
 
   computed: {
     classNames() {
-      const elementName = `pwsp__${this.name}`;
+      const elementName = `pswp__${this.name}`;
       const classNames = [elementName];
       this.modifiers.forEach(mod => classNames.push(`${elementName}--${mod}`));
       return classNames;
