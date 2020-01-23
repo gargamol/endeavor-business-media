5aaf956e6bb0a6822ea8ab7d1a9ba00bf8a8c961
Jacob Bare <jacob@limit0.io>
2020-01-23 11:10:11 -0600

    Add example init and additional markup

--- packages/photoswipe/browser/index.vue
+++ packages/photoswipe/browser/index.vue
@@ -7,15 +7,39 @@
   >
     <pwsp-element name="bg" />
     <pwsp-element name="scroll-wrap">
-      <pwsp-element name="container" />
+      <pwsp-element name="container">
+        <pwsp-item />
+        <pwsp-item />
+        <pwsp-item />
+      </pwsp-element>
+    </pwsp-element>
+
+    <pwsp-element name="ui" :modifiers="['hidden']">
+      <pwsp-element name="top-bar">
+        <pwsp-element name="counter" />
+      </pwsp-element>
     </pwsp-element>
   </div>
 </template>
 
 <script>
+import PhotoSwipe from 'photoswipe';
+import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default';
 import PwspElement from './element.vue';
+import PwspItem from './item.vue';
 
 export default {
-  components: { PwspElement },
+  components: { PwspElement, PwspItem },
+
+  mounted() {
+    const options = { index: 0 };
+    const items = [
+      { src: 'https://placekitten.com/600/400', w: 600, h: 400 },
+      { src: 'https://placekitten.com/1200/900', w: 1200, h: 900 },
+    ];
+
+    const gallery = new PhotoSwipe(this.$el, PhotoSwipeUI, items, options);
+    gallery.init();
+  },
 };
 </script>
