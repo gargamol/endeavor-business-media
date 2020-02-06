29faeee4db2c659efa4660c1dc670e68c9f47546
Brandon Krigbaum <brandonbk@gmail.com>
2020-02-06 08:54:00 -0600

    Add & import leaders config

---
+++ sites/ledsmagazine.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Lighting',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.ledsmagazine.com/files/base/ebm/leds/image/static/leds-leaders.png?h=90',
+  },
+};
