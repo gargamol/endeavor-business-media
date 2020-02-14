ea440226790dd554653e215992a0e499a83c5dbc
Brandon Krigbaum <brandonbk@gmail.com>
2020-02-14 08:07:24 -0600

    Add leaders to WTO

---
+++ sites/watertechonline.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Water Technology',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.watertechonline.com/files/base/ebm/wto/image/static/wto-leaders.png?h=90',
+  },
+};
