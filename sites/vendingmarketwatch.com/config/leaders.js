d0fc00841e7e2143bdd2762aceb9e2cfce031818
Brandon Krigbaum <brandonbk@gmail.com>
2020-02-14 08:21:21 -0600

    Enable leaders for VMW

---
+++ sites/vendingmarketwatch.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Vending',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.vendingmarketwatch.com/files/base/cygnus/vmw/image/static/vmw-leaders.png?h=90',
+  },
+};
