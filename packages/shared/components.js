9d8a476e4ef88b7c4db5157010dae25003d2d2de
Jacob Bare <jacob@limit0.io>
2020-02-24 11:57:51 -0500

    Create shared content card deck flow

--- packages/shared/components.js
+++ packages/shared/components.js
@@ -1,5 +1,7 @@
+const SharedContentCardDeckFlow = require('./components/flows/content-card-deck');
 const SharedMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
+  SharedContentCardDeckFlow,
   SharedMagazineIssueArchiveFlow,
 };
