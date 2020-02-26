90265a33339fe7331e55f2837a49569804c80059
Jacob Bare <jacob@limit0.io>
2020-02-26 08:16:44 -0600

    Create initial content load more flows

--- packages/shared/components.js
+++ packages/shared/components.js
@@ -1,7 +1,9 @@
 const SharedContentCardDeckFlow = require('./components/flows/content-card-deck');
+const SharedContentLoadMoreCardDeckListFlow = require('./components/flows/content-load-more/card-deck-list');
 const SharedMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   SharedContentCardDeckFlow,
+  SharedContentLoadMoreCardDeckListFlow,
   SharedMagazineIssueArchiveFlow,
 };
