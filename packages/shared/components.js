7dad43099a4467b661741b5e9b615c6585f59bbc
Jacob Bare <jacob@limit0.io>
2020-02-26 08:24:11 -0600

    Create content load more card deck

--- packages/shared/components.js
+++ packages/shared/components.js
@@ -1,9 +1,11 @@
 const SharedContentCardDeckFlow = require('./components/flows/content-card-deck');
 const SharedContentLoadMoreCardDeckListFlow = require('./components/flows/content-load-more/card-deck-list');
+const SharedContentLoadMoreCardDeckFlow = require('./components/flows/content-load-more/card-deck');
 const SharedMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   SharedContentCardDeckFlow,
   SharedContentLoadMoreCardDeckListFlow,
+  SharedContentLoadMoreCardDeckFlow,
   SharedMagazineIssueArchiveFlow,
 };
