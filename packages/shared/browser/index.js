76d0bf1112ab5f0e4391831c808156fb0a8fb6d4
Jacob Bare <jacob@limit0.io>
2020-02-26 16:11:51 -0600

    Properly export shared browser loader as func

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -1,4 +1,3 @@
-import Browser from '@base-cms/marko-web/browser';
 import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
@@ -8,21 +7,19 @@ import PhotoSwipe from '@base-cms/marko-web-photoswipe/browser';
 import ContactUs from '@endeavor-business-media/package-contact-us/browser';
 import Inquiry from '@endeavor-business-media/package-inquiry/browser';
 import Leaders from '@endeavor-business-media/package-leaders/browser';
+import Directory from '@endeavor-business-media/package-directory/browser';
 import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
-import AutoScroll from './auto-scroll.vue';
 
-// @todo vet this component's usage
-Browser.registerComponent('SharedAutoScroll', AutoScroll);
-
-DefaultTheme(Browser);
-GTM(Browser);
-GAM(Browser);
-GCSE(Browser);
-RevealAd(Browser);
-Inquiry(Browser);
-Leaders(Browser);
-PhotoSwipe(Browser);
-SocialSharing(Browser);
-ContactUs(Browser);
-
-export default Browser;
+export default (Browser) => {
+  DefaultTheme(Browser);
+  GTM(Browser);
+  GAM(Browser);
+  GCSE(Browser);
+  RevealAd(Browser);
+  Inquiry(Browser);
+  Leaders(Browser);
+  PhotoSwipe(Browser);
+  Directory(Browser);
+  SocialSharing(Browser);
+  ContactUs(Browser);
+};
