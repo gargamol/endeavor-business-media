1e1deedb90f2d9ffd4b9defd3948b714ad5699a8
Jacob Bare <jacob@limit0.io>
2020-03-02 07:59:50 -0600

    Use default layout for companies and whitepapers

--- sites/aviationpros.com/server/routes/content.js
+++ sites/aviationpros.com/server/routes/content.js
@@ -1,17 +1,12 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('@endeavor-business-media/package-shared/graphql/fragments/content-page');
-const content = require('@endeavor-business-media/package-shared/templates/content');
 
-const company = require('../templates/content/company');
+const content = require('../templates/content');
 const whitepaper = require('../templates/content/whitepaper');
 
 module.exports = (app) => {
   app.get('/*?whitepaper/:id(\\d{8})*', withContent({
     template: whitepaper,
-    queryFragment,
-  }));
-  app.get('/*?company/:id(\\d{8})*', withContent({
-    template: company,
     queryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
