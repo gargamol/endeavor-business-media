4c3391317cb875b3c8f2b02fa886ed4bfc705a8e
Jacob Bare <jacob@limit0.io>
2020-03-02 11:18:09 -0600

    Use shared leaders section template

--- sites/laserfocusworld.com/server/routes/website-section.js
+++ sites/laserfocusworld.com/server/routes/website-section.js
@@ -1,9 +1,9 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const leadersFragment = require('@endeavor-business-media/package-leaders/graphql/fragments/leaders-section');
 const contactUs = require('@endeavor-business-media/package-shared/templates/website-section/contact-us');
+const leaders = require('@endeavor-business-media/package-shared/templates/website-section/leaders');
 const queryFragment = require('@endeavor-business-media/package-shared/graphql/fragments/website-section-page');
 
-const leaders = require('../templates/website-section/leaders');
 const directory = require('../templates/website-section/directory');
 const section = require('../templates/website-section');
 const whitePapers = require('../templates/website-section/white-papers');
