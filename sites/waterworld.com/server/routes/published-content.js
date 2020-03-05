91234be05cdb4663943778d89e6ab57041657a90
Jacob Bare <jacob@limit0.io>
2020-03-05 07:23:30 -0600

    Use shared published content templates

--- sites/waterworld.com/server/routes/published-content.js
+++ sites/waterworld.com/server/routes/published-content.js
@@ -1,7 +1,7 @@
-const events = require('../templates/published-content/events');
-const webinars = require('../templates/published-content/webinars');
-const whitePapers = require('../templates/published-content/white-papers');
-const videos = require('../templates/published-content/videos');
+const webinars = require('@endeavor-business-media/package-shared/templates/published-content/webinars');
+const events = require('@endeavor-business-media/package-shared/templates/published-content/events');
+const videos = require('@endeavor-business-media/package-shared/templates/published-content/videos');
+const whitePapers = require('@endeavor-business-media/package-shared/templates/published-content/white-papers');
 
 module.exports = (app) => {
   app.get('/events', (_, res) => { res.marko(events); });
