d1d24e135d375f3a351f05dea0ca2273f6ad305e
Alysha Buselt <>
2020-03-09 15:23:19 -0500

    Setup MAE leaders
    https://www.pivotaltracker.com/story/show/171256934

---
+++ sites/militaryaerospace.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Defense Electonics & Optoelectronics',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.militaryaerospace.com/files/base/ebm/mae/image/static/MAE_Leaders_Final.png?h=90',
+  },
+};
