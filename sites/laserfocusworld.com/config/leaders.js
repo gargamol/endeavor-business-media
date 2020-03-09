ba1a8e659ee21e8a1d2720588bed67b36bfa4421
Alysha Buselt <>
2020-03-09 13:57:18 -0500

    Setup leaders for LFW

---
+++ sites/laserfocusworld.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Lasers and Photonics',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.laserfocusworld.com/files/base/ebm/lfw/image/static/LFW_Leaders.png?h=90',
+  },
+};
