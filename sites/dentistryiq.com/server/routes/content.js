de0ef79f8f2786c75010642ccb9ceafa2ef236ef
Alysha Buselt <>
2020-03-12 09:39:20 -0500

    Rollup of DIQ to shared package & enl info update
    https://www.pivotaltracker.com/story/show/171677227
    and
    https://www.pivotaltracker.com/story/show/171698707

--- sites/dentistryiq.com/server/routes/content.js
+++ sites/dentistryiq.com/server/routes/content.js
@@ -1,16 +1,11 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
+const queryFragment = require('@endeavor-business-media/package-shared/graphql/fragments/content-page');
 const content = require('../templates/content');
-const company = require('../templates/content/company');
 const whitepaper = require('../templates/content/whitepaper');
-const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
   app.get('/*?whitepaper/:id(\\d{8})*', withContent({
     template: whitepaper,
-    queryFragment,
-  }));
-  app.get('/*?company/:id(\\d{8})*', withContent({
-    template: company,
     queryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
