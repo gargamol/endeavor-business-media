a469c4ff5790adb44e8690177dbb0b70170a634e
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-12 09:43:38 -0500

    Update browser index

--- sites/ledsmagazine.com/browser/index.js
+++ sites/ledsmagazine.com/browser/index.js
@@ -1,24 +1,6 @@
 import Browser from '@base-cms/marko-web/browser';
-import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
-import GTM from '@base-cms/marko-web-gtm/browser';
-import GAM from '@base-cms/marko-web-gam/browser';
-import GCSE from '@base-cms/marko-web-gcse/browser';
-import RevealAd from '@base-cms/marko-web-reveal-ad/browser';
-import Common from '@endeavor-business-media/package-common/browser';
-import Inquiry from '@endeavor-business-media/package-inquiry/browser';
-import Leaders from '@endeavor-business-media/package-leaders/browser';
-import PhotoSwipe from '@endeavor-business-media/package-photoswipe/browser';
-import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
+import Shared from '@endeavor-business-media/package-shared/browser';
 
-DefaultTheme(Browser);
-GTM(Browser);
-GAM(Browser);
-GCSE(Browser);
-RevealAd(Browser);
-Common(Browser);
-Inquiry(Browser);
-Leaders(Browser);
-PhotoSwipe(Browser);
-SocialSharing(Browser);
+Shared(Browser);
 
 export default Browser;
