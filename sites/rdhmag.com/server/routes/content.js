81f3dfe6abf1026d5faaa91836cee8d7643aee4a
Alysha Buselt <>
2020-03-19 12:27:55 -0500

    Rollup of RDH to shared package
    https://www.pivotaltracker.com/story/show/171697360

--- sites/rdhmag.com/server/routes/content.js
+++ sites/rdhmag.com/server/routes/content.js
@@ -1,17 +1,22 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
+const queryFragment = require('@endeavor-business-media/package-shared/graphql/fragments/content-page');
+const companyQueryFragment = require('@endeavor-business-media/package-shared/graphql/fragments/content-company-page');
+const noAdsLoadMore = require('@endeavor-business-media/package-shared/templates/content/no-ads-load-more');
+const company = require('@endeavor-business-media/package-shared/templates/content/company');
 const content = require('../templates/content');
-const company = require('../templates/content/company');
-const whitepaper = require('../templates/content/whitepaper');
-const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
+  app.get('/*?webinar/:id(\\d{8})*', withContent({
+    template: noAdsLoadMore,
+    queryFragment,
+  }));
   app.get('/*?whitepaper/:id(\\d{8})*', withContent({
-    template: whitepaper,
+    template: noAdsLoadMore,
     queryFragment,
   }));
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment,
+    queryFragment: companyQueryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
