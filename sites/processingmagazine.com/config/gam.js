8067d8ed9558c51a01a12285832b3ed7c213824d
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-19 15:35:16 -0500

    Update GAM configs

--- sites/processingmagazine.com/config/gam.js
+++ sites/processingmagazine.com/config/gam.js
@@ -1,6 +1,6 @@
-const GAMConfiguration = require('@base-cms/marko-web-gam/config');
+const configureGAM = require('@endeavor-business-media/package-shared/config/gam');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'PCM' });
+const config = configureGAM({ basePath: 'PCM' });
 
 config
   .setTemplate('LB1', {
