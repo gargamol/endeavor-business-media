c96684f0138fa7f9494830d7ff17e6c27a8f4cea
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-24 09:32:51 -0500

    Update route index files

--- sites/masstransitmag.com/server/routes/index.js
+++ sites/masstransitmag.com/server/routes/index.js
@@ -1,5 +1,3 @@
-const contactUsHandler = require('@endeavor-business-media/package-common/contact-us');
-const loadInquiry = require('@endeavor-business-media/package-inquiry/load-from-config');
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
@@ -10,14 +8,6 @@ const subscribe = require('./subscribe');
 const websiteSections = require('./website-section');
 
 module.exports = (app) => {
-  // Handle submissions on /__inquiry
-  // @todo This should be removed once inquiry is moved to core.
-  loadInquiry(app);
-
-  // Handle contact is submissions on /__contact-us
-  // @todo This should be removed once contact-us is moved to core.
-  contactUsHandler(app);
-
   // Homepage
   home(app);
 
