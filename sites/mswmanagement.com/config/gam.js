6451e2c19ac3b08b2b9f6b146b456559d9b1a652
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-31 15:28:38 -0500

    Update GAM configs

--- sites/mswmanagement.com/config/gam.js
+++ sites/mswmanagement.com/config/gam.js
@@ -1,6 +1,6 @@
-const GAMConfiguration = require('@base-cms/marko-web-gam/config');
+const configureGAM = require('@endeavor-business-media/package-shared/config/gam');
 
-const config = new GAMConfiguration('21687441225', { basePath: 'MSW' });
+const config = configureGAM({ basePath: 'MSW' });
 
 config
   .setTemplate('LB1', {
