b0242ad95e13784bc76d1ab29a563f9bbbc2c680
Brandon Krigbaum <brandonbk@gmail.com>
2020-04-10 10:48:52 -0500

    Update search config

--- sites/taxpracticeadvisor.com/config/gcse.js
+++ sites/taxpracticeadvisor.com/config/gcse.js
@@ -1,3 +1,3 @@
 const configureGCSE = require('@endeavor-business-media/package-shared/config/gcse');
 
-module.exports = configureGCSE({ apiKey: '017383739850048358259:fdbdgb9t5h0' });
+module.exports = configureGCSE({ apiKey: '017383739850048358259:cqxhfpcmuhc' });
