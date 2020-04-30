a2d5058da49bfa2e24c43656e8dcbe8ac0f98986
Alysha Buselt <>
2020-04-30 17:32:34 -0400

    Update advertise links for FCN and PM
    https://www.pivotaltracker.com/story/show/172547055

--- sites/processingmagazine.com/config/navigation.js
+++ sites/processingmagazine.com/config/navigation.js
@@ -27,7 +27,7 @@ module.exports = {
     items: [
       { href: '/page/about-us', label: 'About Us' },
       { href: '/contact-us', label: 'Contact Us' },
-      { href: 'https://www.processflownetwork.com', label: 'Advertise', target: '_blank' },
+      { href: 'https://industrial.endeavorb2b.com/processing/', label: 'Advertise', target: '_blank' },
       { href: 'https://endeavor.dragonforms.com/loading.do?omedasite=EBM_DoNotSell', label: 'California Do Not Sell', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
@@ -58,7 +58,7 @@ module.exports = {
       label: 'User Tools',
       items: [
         { href: '/subscribe', label: 'Subscribe' },
-        { href: 'https://www.processflownetwork.com', label: 'Advertise', target: '_blank' },
+        { href: 'https://industrial.endeavorb2b.com/processing/', label: 'Advertise', target: '_blank' },
         { href: '/page/about-us', label: 'About Us' },
         { href: '/contact-us', label: 'Contact Us' },
         { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
