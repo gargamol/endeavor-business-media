7319ca5363fb6cdcd43cc7278f93480c36c037aa
Alysha Buselt <>
2020-04-30 12:09:23 -0400

    Setup leaders for Officer
    https://www.pivotaltracker.com/story/show/172591534

---
+++ sites/officer.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Law Enforcement',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.officer.com/files/base/cygnus/ofcr/image/static/leaders in law enforcement with badge.png?h=90',
+  },
+};
