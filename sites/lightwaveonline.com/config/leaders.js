7de04a14bf6b580e735cbd8008f55cbe6cdf2f1b
Alysha Buselt <>
2020-05-21 15:23:23 -0400

    Setup leaders for Lightwave
    https://www.pivotaltracker.com/story/show/172923977

---
+++ sites/lightwaveonline.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Optical Communications',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.lightwaveonline.com/files/base/ebm/lw/image/static/lw-leaders.png?h=90',
+  },
+};
