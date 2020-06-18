d37dcd269867be6c875b0d182b70c8b519bead4c
Alysha Buselt <>
2020-06-18 09:16:23 -0400

    Corrections to WW

--- sites/waterworld.com/config/dragon-forms.js
+++ sites/waterworld.com/config/dragon-forms.js
@@ -4,6 +4,7 @@ const config = new DragonFormsConfig({ url: 'https://endeavor.dragonforms.com' }
 
 config
   .addForm('doNotSell', { omedasite: 'EBM_DoNotSell' })
+  .addForm('newsletterPref', { omedasite: 'WWPrefPage' })
   .addForm('newsletterSubscribe', { omedasite: 'WWNewPref' })
   .addForm('newsletterSignup', { omedasite: 'WWNewPref', query: { pk: 'ARTWEB' } });
 module.exports = config;
