e6b79adbf579707251f9f1bb14c1ac9f02c08b83
Alysha Buselt <>
2020-06-18 09:34:21 -0400

    Update officer forms

--- sites/officer.com/config/dragon-forms.js
+++ sites/officer.com/config/dragon-forms.js
@@ -3,6 +3,8 @@ const DragonFormsConfig = require('@endeavor-business-media/package-shared/confi
 const config = new DragonFormsConfig({ url: 'https://endeavor.dragonforms.com' });
 
 config
-  .addForm('newsletterSignup', { omedasite: 'Officernewpref' });
-
+  .addForm('doNotSell', { omedasite: 'EBM_DoNotSell' })
+  .addForm('newsletterPref', { omedasite: 'OfficerPrefPage' })
+  .addForm('newsletterSubscribe', { omedasite: 'OfficerNewPref' })
+  .addForm('newsletterSignup', { omedasite: 'OfficerNewPref', query: { pk: 'ARTWEB' } });
 module.exports = config;
