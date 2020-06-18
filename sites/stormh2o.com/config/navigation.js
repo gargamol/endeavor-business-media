7419794217027a475fab40bd9930d88ac8cab04f
Alysha Buselt <>
2020-06-18 09:11:25 -0400

    Update SW forms

--- sites/stormh2o.com/config/navigation.js
+++ sites/stormh2o.com/config/navigation.js
@@ -1,3 +1,5 @@
+const dragonForms = require('./dragon-forms');
+
 module.exports = {
   primary: {
     items: [
@@ -26,7 +28,7 @@ module.exports = {
       { href: '/page/about-us', label: 'About Us' },
       { href: '/contact-us', label: 'Contact Us' },
       { href: '/page/advertise', label: 'Advertise' },
-      { href: 'https://endeavor.dragonforms.com/loading.do?omedasite=EBM_DoNotSell', label: 'California Do Not Sell', target: '_blank' },
+      { href: dragonForms.getFormUrl('doNotSell'), label: 'California Do Not Sell', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
     ],
@@ -48,6 +50,7 @@ module.exports = {
         { href: '/events', label: 'Events' },
         { href: '/magazine', label: 'Magazine' },
         { href: '/white-papers', label: 'Free Reports' },
+        { href: dragonForms.getFormUrl('newsletterSubscribe'), label: 'eNewsletter', target: '_blank' },
       ],
     },
     {
