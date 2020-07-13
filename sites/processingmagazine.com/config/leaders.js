63a23648fe0fe19d75875ea6a6e7e46c1642c41a
Alysha Buselt <>
2020-07-13 12:40:37 -0400

    Setup leaders for PM
    https://www.pivotaltracker.com/story/show/173775253

---
+++ sites/processingmagazine.com/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leaders in Process Industry Solutions',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.processingmagazine.com/files/base/ebm/pm/image/static/pm-leaders.png?h=60',
+  },
+};
