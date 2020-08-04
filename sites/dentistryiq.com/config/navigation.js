fc82e64ed06c8203b13874c89fd249135b8cf65f
Alysha Buselt <>
2020-08-04 10:42:58 -0400

    Update “Dental Jobs” to “Dental Works” on DIQ nav
    https://www.pivotaltracker.com/story/show/174102490

--- sites/dentistryiq.com/config/navigation.js
+++ sites/dentistryiq.com/config/navigation.js
@@ -9,7 +9,7 @@ module.exports = {
       { href: '/front-office', label: 'Front Office' },
       { href: '/dental-assisting', label: 'Dental Assisting' },
       { href: '/practice-management', label: 'Practice Management' },
-      { href: '/dental-jobs', label: 'Dental Jobs' },
+      { href: '/dental-works', label: 'Dental Works' },
     ],
   },
   secondary: {
@@ -46,7 +46,7 @@ module.exports = {
         { href: '/front-office', label: 'Front Office' },
         { href: '/dental-assisting', label: 'Dental Assisting' },
         { href: '/practice-management', label: 'Practice Management' },
-        { href: '/dental-jobs', label: 'Dental Jobs' },
+        { href: '/dental-Works', label: 'Dental Works' },
       ],
     },
     {
